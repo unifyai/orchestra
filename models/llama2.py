68cc833d5c18a2063dbb272c268c6f674b1059e6
kurshakuz <kurshakuz@users.noreply.github.com>
2023-11-29 12:43:07 +0000

    refactor llama2 model to general model

--- models/llama2.py
+++ models/llama2.py
@@ -4,55 +4,35 @@ from providers.completion.perplexity import Perplexity
 from providers.completion.together_ai import TogetherAI
 
 
-llama2_7b_chat = {
-    "anyscale": "anyscale/meta-llama/Llama-2-7b-chat-hf",
-    "perplexity": "perplexity/pplx-7b-chat-alpha",
-    "together_ai": "together_ai/togethercomputer/llama-2-7b"
-}
+class Llama2Chat:
+    def __init__(self, provider, model):
+        supported_providers = {
+            "anyscale": {
+                "7b": "anyscale/meta-llama/Llama-2-7b-chat-hf",
+                "13b": "anyscale/meta-llama/Llama-2-13b-chat-hf",
+                "70b": "anyscale/meta-llama/Llama-2-70b-chat-hf"
+            },
+            "perplexity": {
+                "7b": "perplexity/pplx-7b-chat-alpha",
+                "13b": "perplexity/llama-2-13b-chat",
+                "70b": "perplexity/llama-2-70b-chat"
+            },
+            "replicate": {
+                "7b": "replicate/llama-2-7b-chat",
+                "13b": "replicate/a16z-infra/llama-2-13b-chat",
+                "70b": "replicate/llama-2-70b-chat"
+            },
+            "together_ai": {
+                "7b": "together_ai/togethercomputer/llama-2-7b",
+                "70b": "together_ai/togethercomputer/llama-2-70b"
+            }
+        }
 
-llama2_13b_chat = {
-    "anyscale": "anyscale/meta-llama/Llama-2-13b-chat-hf",
-    "perplexity": "perplexity/llama-2-13b-chat",
-    "replicate": "replicate/a16z-infra/llama-2-13b-chat"
-}
+        if provider not in supported_providers:
+            raise Exception("Unsupported provider")
 
-llama2_70b_chat = {
-    "anyscale": "anyscale/meta-llama/Llama-2-70b-chat-hf",
-    "perplexity": "perplexity/llama-2-70b-chat",
-    "replicate": "replicate/llama-2-70b-chat",
-    "together_ai": "together_ai/togethercomputer/llama-2-70b-chat",
-}
-
-
-class Llama7BChat:
-    def __init__(self, provider):
-        if provider not in llama2_7b_chat:
-            raise Exception("Invalid model")
-
-        if provider == "anyscale":
-            self.provider_obj = Anyscale()
-        elif provider == "perplexity":
-            self.provider_obj = Perplexity()
-        elif provider == "replicate":
-            self.provider_obj = Replicate()
-        elif provider == "together_ai":
-            self.provider_obj = TogetherAI()
-        else:
-            raise Exception("Invalid provider")
-
-        self.provider_obj.model = llama2_7b_chat[provider]
-
-    def set_api_key(self, api_key):
-        self.provider_obj.set_api_key(api_key)
-
-    def get_completion(self, prompt, max_tokens=16, temperature=0.9):
-        return self.provider_obj.complete(prompt, max_tokens, temperature)
-
-
-class Llama70BChat:
-    def __init__(self, provider):
-        if provider not in llama2_70b_chat:
-            raise Exception("Invalid model")
+        if model not in supported_providers[provider]:
+            raise Exception("Unsupported model")
 
         if provider == "anyscale":
             self.provider_obj = Anyscale()
@@ -65,7 +45,7 @@ class Llama70BChat:
         else:
             raise Exception("Invalid provider")
 
-        self.provider_obj.model = llama2_70b_chat[provider]
+        self.provider_obj.model = supported_providers[provider][model]
 
     def set_api_key(self, api_key):
         self.provider_obj.set_api_key(api_key)
