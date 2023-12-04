49df9d711e1495e18550a849fb6f060c62533980
aparajith21 <58738384+aparajith21@users.noreply.github.com>
2023-12-04 16:24:48 +0000

    updates to schema, views

--- orchestra/web/api/chat_completion/schema.py
+++ orchestra/web/api/chat_completion/schema.py
@@ -1,21 +1,23 @@
-from pydantic import BaseModel, ConfigDict
+from typing import List
+from pydantic import BaseModel
 
 
 class ChatCompletionRequest(BaseModel):
     """
-    chat completion based language model requests
+    Chat completion based language model requests.
     """
     model: str
     messages: List[dict]
     temperature: float
 
+
 class ChatCompletionResponse(BaseModel):
     """
-    chat completion based language model responses
+    Chat completion based language model responses.
     """
     model: str
     created: float
     id: str
-    object: str =  "chat.completion"
+    object: str = "chat.completion"
     usage: dict
     choices: List[dict]
--- orchestra/web/api/chat_completion/views.py
+++ orchestra/web/api/chat_completion/views.py
@@ -1,23 +1,31 @@
 from typing import List
 
 from fastapi import APIRouter
-from fastapi.param_functions import Depends
-
 from orchestra.web.api.schema import CompletionRequest, CompletionResponse
+from models import Llama2Chat, Mistral
 
 router = APIRouter()
 
+def get_chat_model(request):
+    if "llama" in request.model.split("/")[-1]:
+        return Llama2Chat(provider=request.model.split("/")[0], model=request.model)
+    elif "mistral" in request.model.split("/")[-1]:
+        return Mistral(provider=request.model.split("/")[0], model=request.model)
+
 @router.post("/chat/completion", response_model=CompletionResponse)
-async def get_completions(
-    request: CompletionRequest
-) -> CompletionResponse:
-
-    language_model = Llama2Chat(provider=request.model.split("/")[0], model=request.model)
+async def get_completions(request: CompletionRequest) -> CompletionResponse:
+    language_model = get_chat_model(request)
     
-    response = llama_model.get_completion(messages=request.messages, temperature=request.temperature)
+    response = language_model.get_completion(messages=request.messages, temperature=request.temperature)
 
-    returned_response = CompletionResponse(model=request.model, created=response["created"], choices=response["choices"],
-                        object=response["object"], usage=response["usage"])
+    returned_response = CompletionResponse(
+        model=request.model,
+        created=response["created"],
+        choices=response["choices"],
+        object=response["object"],
+        usage=response["usage"]
+    )
+    
     completions = generate_completions(request.model, request.messages, request.temperature)
 
     return returned_response
