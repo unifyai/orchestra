c0aded0df6db2711acbbf16a1d5a22653a854672
JG <67888047+juliagsy@users.noreply.github.com>
2023-12-04 11:03:53 +0000

    feat: orchestra auth logic (#2)
    * create auth function
    
    * test protect on some dummy endpoints
    
    * comment out endpoint protection for now

--- orchestra/web/api/router.py
+++ orchestra/web/api/router.py
@@ -1,8 +1,21 @@
+from fastapi import Depends
 from fastapi.routing import APIRouter
 
 from orchestra.web.api import dummy, echo, monitoring
+from orchestra.web.api.dependencies import auth_api_key
+
+AUTH = [Depends(auth_api_key)]
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
-api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
-api_router.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
+api_router.include_router(
+    echo.router,
+    prefix="/echo",
+    tags=["echo"],
+)
+api_router.include_router(
+    dummy.router,
+    prefix="/dummy",
+    tags=["dummy"],
+    # dependencies=AUTH, # noqa: E800
+)
