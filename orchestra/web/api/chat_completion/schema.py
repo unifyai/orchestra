4ce7f87d365a7b2a395f1d221d54e25900be153f
aparajith21 <58738384+aparajith21@users.noreply.github.com>
2023-12-04 17:13:33 +0000

    fix: lint

--- orchestra/web/api/chat_completion/schema.py
+++ orchestra/web/api/chat_completion/schema.py
@@ -1,25 +1,39 @@
-from typing import List
+from typing import Dict, List
+
 from pydantic import BaseModel
 
 
 class ChatCompletionRequest(BaseModel):
     """
-    Chat completion based language model requests.
+    Request model for chat completion based language model.
+
+    Attributes:
+        model (str): The model identifier.
+        messages (List[Dict]): List of messages for completion.
+        temperature (float): The temperature parameter for generating completions.
     """
 
     model: str
-    messages: List[dict]
+    messages: List[Dict]
     temperature: float
 
 
 class ChatCompletionResponse(BaseModel):
     """
-    Chat completion based language model responses.
+    Response model for chat completion based language model.
+
+    Attributes:
+        model (str): The model identifier.
+        created (float): Timestamp indicating when the response was created.
+        id (str): Identifier for the completion response.
+        object (str): The type of object, defaults to "chat.completion".
+        usage (dict): Usage statistics or additional information.
+        choices (List[Dict]): List of completion choices.
     """
 
     model: str
     created: float
     id: str
     object: str = "chat.completion"
-    usage: dict
-    choices: List[dict]
+    usage: Dict
+    choices: List[Dict]
--- orchestra/web/api/chat_completion/views.py
+++ orchestra/web/api/chat_completion/views.py
@@ -1,13 +1,21 @@
-from typing import List
-
 from fastapi import APIRouter
-from orchestra.web.api.schema import CompletionRequest, CompletionResponse
 from models import Llama2Chat, Mistral
+
+from orchestra.web.api.schema import CompletionRequest, CompletionResponse
 
 router = APIRouter()
 
 
 def get_chat_model(request):
+    """
+    Get the chat model based on the request.
+
+    :param request: CompletionRequest object.
+    :type request: CompletionRequest
+
+    :return: Chat model instance (Llama2Chat or Mistral).
+    :rtype: Union[Llama2Chat, Mistral]
+    """
     if "llama" in request.model.split("/")[-1]:
         return Llama2Chat(provider=request.model.split("/")[0], model=request.model)
     elif "mistral" in request.model.split("/")[-1]:
@@ -16,22 +24,24 @@ def get_chat_model(request):
 
 @router.post("/chat/completion", response_model=CompletionResponse)
 async def get_completions(request: CompletionRequest) -> CompletionResponse:
+    """
+    Get chat completions based on the request.
+
+    :param request: CompletionRequest object.
+
+    :return: CompletionResponse object.
+    """
     language_model = get_chat_model(request)
 
     response = language_model.get_completion(
-        messages=request.messages, temperature=request.temperature
+        messages=request.messages,
+        temperature=request.temperature,
     )
 
-    returned_response = CompletionResponse(
+    return CompletionResponse(
         model=request.model,
         created=response["created"],
         choices=response["choices"],
         object=response["object"],
         usage=response["usage"],
     )
-
-    completions = generate_completions(
-        request.model, request.messages, request.temperature
-    )
-
-    return returned_response
