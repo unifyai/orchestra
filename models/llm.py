a7a2b0c3e70d8bdf7d4eb829b5ffd8ec7d28e328
Rishab Mallick <rishabmallick6@gmail.com>
2023-12-05 13:41:09 +0000

    refactor: PROVIDER_CLASSES is global now

--- models/llm.py
+++ models/llm.py
@@ -2,24 +2,24 @@ from providers.completion.anyscale import Anyscale
 from providers.completion.perplexity import Perplexity
 from providers.completion.together_ai import TogetherAI
 
+PROVIDER_CLASSES = {
+    "anyscale": Anyscale,
+    "perplexity": Perplexity,
+    "together_ai": TogetherAI,
+}
+
 
 class CompletionsModel:
     """Sets up a general CompletionsModel service."""
 
-    PROVIDER_CLASSES = {
-        "anyscale": Anyscale,
-        "perplexity": Perplexity,
-        "together_ai": TogetherAI,
-    }
-
     def __init__(self, provider: str, model: str) -> None:
-        if provider.lower() not in self.PROVIDER_CLASSES:
+        if provider.lower() not in PROVIDER_CLASSES:
             raise Exception("Provider not supported by Unify")  # noqa: WPS454
 
-        if model.lower() not in self.PROVIDER_CLASSES[provider].supported_models:
+        if model.lower() not in PROVIDER_CLASSES[provider].supported_models:
             raise Exception(f"Model not supported by {provider}")  # noqa: WPS454
 
-        self.provider_obj = self.PROVIDER_CLASSES[provider]()
+        self.provider_obj = PROVIDER_CLASSES[provider]()
         self.model = model.lower()
 
     def set_api_key(self, api_key: str) -> None:  # noqa: D102
