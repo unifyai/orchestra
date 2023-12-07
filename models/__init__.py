675910b11e50aa79320954866d68be8946c1ed1d
aparajith21 <58738384+aparajith21@users.noreply.github.com>
2023-12-07 04:31:45 -0500

    adding chat completion api files (#3)
    * adding chat completion api files
    
    * updates to schema, views
    
    * fixing black lint errors
    
    * fix: lint
    
    * changes post refactor
    
    * add auth logic to chat_completion in master router
    
    * fix: chat_completion tags in router
    
    ---------
    
    Co-authored-by: juliagsy <67888047+juliagsy@users.noreply.github.com>

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+"""
+Requests are sent to different models and providers here based on their types.
+
+This module contains the logic for handling requests to different models and providers.
+It dispatches requests based on the type of models.
+"""
---
+++ orchestra/web/api/chat_completion/views.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter
+from models.llm import CompletionsModel
+
+from orchestra.web.api.chat_completion.schema import (
+    ChatCompletionRequest,
+    ChatCompletionResponse,
+)
+
+router = APIRouter()
+
+
+@router.post("/chat/completion", response_model=ChatCompletionRequest)
+async def get_completions(request: ChatCompletionRequest) -> ChatCompletionResponse:
+    """
+    Get chat completions based on the request.
+
+    :param request: ChatCompletionRequest object.
+
+    :return: ChatCompletionResponse object.
+    """
+    language_model = CompletionsModel(
+        provider=request.model.split("/")[0],
+        model=request.model,
+    )
+
+    response = language_model.get_completion(
+        prompt=request.messages,
+        temperature=request.temperature,
+    )
+    return ChatCompletionResponse(
+        model=request.model,
+        created=response.get("created", None),
+        choices=response.get("choices", None),
+        object=response.get("object", None),
+        usage=response.get("usage", None),
+    )
