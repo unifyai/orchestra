429bc512053c8269855b0784fa7a9d29ee06a59c
Rishab Mallick <rishabmallick6@gmail.com>
2023-12-07 11:17:14 +0000

    chore: updates provider endpoints

--- providers/completion/anyscale.py
+++ providers/completion/anyscale.py
@@ -9,9 +9,10 @@ class Anyscale(BaseCompletionProvider):
     """
 
     supported_models = {
-        "llama-2-7b-chat-hf": "anyscale/meta-llama/Llama-2-7b-chat-hf",
-        "llama-2-13b-chat-hf": "anyscale/meta-llama/Llama-2-13b-chat-hf",
-        "llama-2-70b-chat-hf": "anyscale/meta-llama/Llama-2-70b-chat-hf",
+        "llama-2-7b-chat": "anyscale/meta-llama/Llama-2-7b-chat-hf",
+        "llama-2-13b-chat": "anyscale/meta-llama/Llama-2-13b-chat-hf",
+        "llama-2-70b-chat": "anyscale/meta-llama/Llama-2-70b-chat-hf",
         "mistral-7b-instruct-v0.1": "anyscale/mistralai/Mistral-7B-Instruct-v0.1",
-        "codellama-34b-instruct-hf": "anyscale/codellama/CodeLlama-34b-Instruct-hf",
+        "codellama-34b-instruct": "anyscale/codellama/CodeLlama-34b-Instruct-hf",
+        "zephyr-7b-beta": "anyscale/HuggingFaceH4/zephyr-7b-beta",
     }
--- providers/completion/replicate.py
+++ providers/completion/replicate.py
@@ -9,7 +9,10 @@ class Replicate(BaseCompletionProvider):
     """
 
     supported_models = {
-        "mistral-7b-instruct-v0.1": "replicate/mistralai/mistral-7b-instruct-v0.1",
-        "llama-2-70b-chat": "replicate/meta/llama-2-70b-chat",
-        "gpt-j-6b": "replicate/gpt-j-6b",
+        "mistral-7b-instruct-v0.1": "replicate/mistralai/mistral-7b-instruct-v0.1:83b6a56e7c828e667f21fd596c338fd4f0039b46bcfa18d973e8e70e455fda70",
+        "mistral-7b-v0.1": "replicate/mistralai/mistral-7b-v0.1:3e8a0fb6d7812ce30701ba597e5080689bef8a013e5c6a724fafb108cc2426a0",
+        "llama-2-70b-chat": "replicate/meta/llama-2-70b-chat:02e509c789964a7ea8736978a43525956ef40397be9033abf9fd2badfe68c9e3",
+        "gpt-j-6b": "replicate/gpt-j-6b:b3546aeec6c9891f0dd9929c2d3bedbf013c12e02e7dd0346af09c37e008c827",
+        "llama-2-13b-chat": "replicate/meta/llama-2-13b-chat:f4e2de70d66816a838a89eeeb621910adffb0dd0baba3976c96980970978018d",
+        "llama-2-7b-chat": "replicate/meta/llama-2-7b-chat:13c3cdee13ee059ab779f0291d29054dab00a47dad8261375654de5540165fb0",
     }
