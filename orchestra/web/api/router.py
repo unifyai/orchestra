81bb6858898823781d2c6570c5809df74f712731
guillesanbri <Guillesanbri@hotmail.com>
2023-12-08 00:25:05 +0000

    add models endpoint

--- orchestra/web/api/router.py
+++ orchestra/web/api/router.py
@@ -1,7 +1,7 @@
 from fastapi import Depends
 from fastapi.routing import APIRouter
 
-from orchestra.web.api import chat_completion, dummy, echo, monitoring
+from orchestra.web.api import chat_completion, dummy, echo, models, monitoring
 from orchestra.web.api.dependencies import auth_api_key
 
 AUTH = [Depends(auth_api_key)]
@@ -10,6 +10,7 @@ api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
 api_router.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
+api_router.include_router(models.router, tags=["models"])  # TODO: Add auth
 api_router.include_router(
     chat_completion.router,
     tags=["chat_completion"],
