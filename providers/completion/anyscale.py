e122a5e67c0fe7e43a201f316a31a5fca8651f56
Rishab Mallick <rishabmallick6@gmail.com>
2023-12-11 13:21:32 +0000

    feat: adds pricing of openai, anthropic, anyscale, perplexity

--- providers/completion/anyscale.py
+++ providers/completion/anyscale.py
@@ -6,13 +6,38 @@ class Anyscale(BaseCompletionProvider):
     A completion provider that uses the Anyscale service.
 
     Source: https://docs.anyscale.com/endpoints/overview#supported-models
+    Pricing is per million tokens: https://docs.endpoints.anyscale.com/pricing
     """
 
     supported_models = {
-        "llama-2-7b-chat": "anyscale/meta-llama/Llama-2-7b-chat-hf",
-        "llama-2-13b-chat": "anyscale/meta-llama/Llama-2-13b-chat-hf",
-        "llama-2-70b-chat": "anyscale/meta-llama/Llama-2-70b-chat-hf",
-        "mistral-7b-instruct-v0.1": "anyscale/mistralai/Mistral-7B-Instruct-v0.1",
-        "codellama-34b-instruct": "anyscale/codellama/CodeLlama-34b-Instruct-hf",
-        "zephyr-7b-beta": "anyscale/HuggingFaceH4/zephyr-7b-beta",
+        "llama-2-7b-chat": {
+            "endpoint": "anyscale/meta-llama/Llama-2-7b-chat-hf",
+            "context_window": 4096,
+            "cost": {"prompt": 0.15, "completion": 0.15},
+        },
+        "llama-2-13b-chat": {
+            "endpoint": "anyscale/meta-llama/Llama-2-13b-chat-hf",
+            "context_window": 4096,
+            "cost": {"prompt": 0.25, "completion": 0.25},
+        },
+        "llama-2-70b-chat": {
+            "endpoint": "anyscale/meta-llama/Llama-2-70b-chat-hf",
+            "context_window": 4096,
+            "cost": {"prompt": 1, "completion": 1},
+        },
+        "mistral-7b-instruct-v0.1": {
+            "endpoint": "anyscale/mistralai/Mistral-7B-Instruct-v0.1",
+            "context_window": 16384,
+            "cost": {"prompt": 0.15, "completion": 0.15},
+        },
+        "codellama-34b-instruct": {
+            "endpoint": "anyscale/codellama/CodeLlama-34b-Instruct-hf",
+            "context_window": 16384,
+            "cost": {"prompt": 1, "completion": 1},
+        },
+        "zephyr-7b-beta": {
+            "endpoint": "anyscale/HuggingFaceH4/zephyr-7b-beta",
+            "context_window": 16384,
+            "cost": {"prompt": 0.15, "completion": 0.15},
+        },
     }
--- providers/completion/perplexity.py
+++ providers/completion/perplexity.py
@@ -6,14 +6,51 @@ class Perplexity(BaseCompletionProvider):
     A completion provider that uses the Perplexity service.
 
     Supported models: https://docs.perplexity.ai/docs/model-cards
+    Pricing is per million tokens: https://docs.perplexity.ai/docs/pricing
     """
 
     supported_models = {
-        "codellama-34b-instruct": "perplexity/codellama-34b-instruct",
-        "llama-2-70b-chat": "perplexity/llama-2-70b-chat",
-        "mistral-7b-instruct": "perplexity/mistral-7b-instruct",
-        "pplx-7b-chat": "perplexity/pplx-7b-chat",
-        "pplx-70b-chat": "perplexity/pplx-70b-chat",
-        "pplx-7b-online": "perplexity/pplx-7b-online",
-        "pplx-70b-online": "perplexity/pplx-70b-online",
+        "codellama-34b-instruct": {
+            "endpoint": "perplexity/codellama-34b-instruct",
+            "context_window": 16384,
+            "cost": {"prompt": 0.35, "completion": 1.40},
+        },
+        "llama-2-70b-chat": {
+            "endpoint": "perplexity/llama-2-70b-chat",
+            "context_window": 4096,
+            "cost": {"prompt": 0.70, "completion": 2.80},
+        },
+        "mistral-7b-instruct": {
+            "endpoint": "perplexity/mistral-7b-instruct",
+            "context_window": 4096,
+            "cost": {"prompt": 0.07, "completion": 0.28},
+        },
+        "pplx-7b-chat": {
+            "endpoint": "perplexity/pplx-7b-chat",
+            "context_window": 8192,
+            "cost": {"prompt": 0.07, "completion": 0.28},
+        },
+        "pplx-70b-chat": {
+            "endpoint": "perplexity/pplx-70b-chat",
+            "context_window": 4096,
+            "cost": {"prompt": 0.70, "completion": 2.80},
+        },
+        "pplx-7b-online": {
+            "endpoint": "perplexity/pplx-7b-online",
+            "context_window": 4096,
+            "cost": {
+                "prompt": 0,
+                "completion": 0.28,
+                "online": {"charge_per_1000_requests": 5},
+            },
+        },
+        "pplx-70b-online": {
+            "endpoint": "perplexity/pplx-70b-online",
+            "context_window": 4096,
+            "cost": {
+                "prompt": 0,
+                "completion": 2.80,
+                "online": {"charge_per_1000_requests": 5},
+            },
+        },
     }
