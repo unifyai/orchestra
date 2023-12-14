c4ba900a50467fa625a063d31b892db97ceb91bb
Rishab Mallick <rishabmallick6@gmail.com>
2023-12-14 23:28:37 +0530

    Benchmarking Models Across Providers (#6)
    * docs: adds prettytable in poetry
    
    * fix: pre-commit to run on staged files now...
    instead of all
    
    * chore: refactors PROVIDER_CLASSES as import
    
    * feat: adds problems db for benchmark
    
    * chore: updates provider endpoints
    
    * feat: adds main benchmarking script
    
    * refactor: deals w all pre-commit lints
    
    * chore: fixes mypy lints
    
    * chore: lint + merge artifacts
    
    * chore: adds `pass_filenames: false` in precommit
    
    * feat: adds pricing of openai, anthropic, anyscale, perplexity
    
    * feat: adds context windows in togetherai
    
    * feat: adds context window for vertexai llms
    
    * feat: adds pricing and login for vertexai llms
    
    * fix: vertex endpoint update
    
    * feat: updates + refactors on providers metadata
    
    * chore: flake8 skips gcloud directory
    
    * feat: adds cost and context window in benchmarking
    
    * fix: total token calc fix and misc chores
    
    * chore: lint fixes, readme for benchmarking steps
    
    * chore: mypy lints
    
    * chore: adds misc data for easier reading
    
    * feat: adds cold start latency + misc chores

--- providers/completion/anthropic.py
+++ providers/completion/anthropic.py
@@ -6,11 +6,28 @@ class Anthropic(BaseCompletionProvider):
     A completion provider that uses the Anthropic service.
 
     Supported models: https://docs.anthropic.com/claude/reference/selecting-a-model
+    Pricing is per million tokens.
     """
 
     supported_models = {
-        "claude-2.1",
-        "claude-2",
-        "claude-instant-1",
-        "claude-instant-1.2",
+        "claude-2.1": {
+            "endpoint": "claude-2.1",
+            "context_window": 200000,
+            "cost": {"prompt": 8, "completion": 24},
+        },
+        "claude-2": {
+            "endpoint": "claude-2",
+            "context_window": 100000,
+            "cost": {"prompt": 8, "completion": 24},
+        },
+        "claude-instant-1": {
+            "endpoint": "claude-instant-1",
+            "context_window": 100000,
+            "cost": {"prompt": 0.8, "completion": 2.4},
+        },
+        "claude-instant-1.2": {
+            "endpoint": "claude-instant-1.2",
+            "context_window": 100000,
+            "cost": {"prompt": 0.8, "completion": 2.4},
+        },
     }
