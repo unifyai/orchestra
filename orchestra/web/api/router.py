40a72c4e2c2f8d162c7d7eb46bc602eeae282d32
kurshakuz <abilkasov@gmail.com>
2023-12-18 17:08:37 +0100

    user ops endpoints (#17)
    * delete dummy files
    
    * fix poetry
    
    * add user ops endpoints
    
    * remove deprecated test

--- orchestra/web/api/router.py
+++ orchestra/web/api/router.py
@@ -1,15 +1,14 @@
 from fastapi import Depends
 from fastapi.routing import APIRouter
 
-from orchestra.web.api import chat_completion, dummy, echo, monitoring
+from orchestra.web.api import chat_completion, monitoring, user
 from orchestra.web.api.dependencies import auth_api_key
 
 AUTH = [Depends(auth_api_key)]
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
-api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
-api_router.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
+api_router.include_router(user.router, prefix="/user", tags=["user"])
 api_router.include_router(
     chat_completion.router,
     tags=["chat_completion"],
