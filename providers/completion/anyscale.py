f6481ba5b5e79385e66c68d96585e0a8c7d9ebf4
aparajith21 <58738384+aparajith21@users.noreply.github.com>
2023-12-25 06:30:26 +0000

    Fix: model-provider string split + anyscale

--- providers/completion/anyscale.py
+++ providers/completion/anyscale.py
@@ -10,17 +10,17 @@ class Anyscale(BaseCompletionProvider):
     """
 
     supported_models = {
-        "llama-2-7b-chat": {
+        "llama-2-7b-chat-hf": {
             "endpoint": "anyscale/meta-llama/Llama-2-7b-chat-hf",
             "context_window": 4096,
             "cost": {"prompt": 0.15, "completion": 0.15},
         },
-        "llama-2-13b-chat": {
+        "llama-2-13b-chat-hf": {
             "endpoint": "anyscale/meta-llama/Llama-2-13b-chat-hf",
             "context_window": 4096,
             "cost": {"prompt": 0.25, "completion": 0.25},
         },
-        "llama-2-70b-chat": {
+        "llama-2-70b-chat-hf": {
             "endpoint": "anyscale/meta-llama/Llama-2-70b-chat-hf",
             "context_window": 4096,
             "cost": {"prompt": 1, "completion": 1},
@@ -30,7 +30,7 @@ class Anyscale(BaseCompletionProvider):
             "context_window": 16384,
             "cost": {"prompt": 0.15, "completion": 0.15},
         },
-        "codellama-34b-instruct": {
+        "codellama-34b-instruct-hf": {
             "endpoint": "anyscale/codellama/CodeLlama-34b-Instruct-hf",
             "context_window": 16384,
             "cost": {"prompt": 1, "completion": 1},
