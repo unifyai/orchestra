77671fc2a9314041c6f5d36d43baf37189da63b9
aparajith21 <58738384+aparajith21@users.noreply.github.com>
2024-01-03 19:38:33 +0530

    Image gen endpoint changes based on api reference (#30)
    * adding imagegen endpoint
    
    * black lint fix
    
    * flake8 lint fix
    
    * adding inference/predict endpoint
    
    * predict -> inference
    
    * remove imagege endpoint to use /inference instead
    
    * adding octoAI
    
    * image modifications based on api reference
    
    * standardizing model names
    
    * removing stability sdk

--- providers/image_gen/__init__.py
+++ providers/image_gen/__init__.py
@@ -1,10 +1,8 @@
 """providers.image_gen package."""
+from providers.image_gen.octoai import OctoAI
 from providers.image_gen.stability import Stability
 
 PROVIDER_CLASSES = {
     "stability": Stability,
+    "octoai": OctoAI,
 }
-
-# Pricing info of providers with pay-per-token model is
-# standardized to per million tokens.
-PRICING_PER_TOKENS = 1000000
