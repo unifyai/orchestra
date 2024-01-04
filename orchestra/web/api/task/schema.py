8ee0ff1713c00ab2ff4f31e3e69735dbbaa11786
kurshakuz <abilkasov@gmail.com>
2024-01-04 13:35:11 +0100

    major endpoints revamp + moved the admin access to put statements + protected with auth (#34)

--- orchestra/web/api/task/schema.py
+++ orchestra/web/api/task/schema.py
@@ -1,19 +1,6 @@
 from pydantic import BaseModel
 
 
-class TaskModelRequest(BaseModel):
-    """
-    Request model for creating new task model.
-
-    Attributes:
-        name (str): The name of the task.
-        modality (str): The modality of the task.
-    """
-
-    name: str
-    modality: str
-
-
 class TaskModelResponse(BaseModel):
     """
     Response model for task models.
