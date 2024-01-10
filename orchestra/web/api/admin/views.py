d40a434e872a374b8588c1b00457f29caa1def21
kurshakuz <abilkasov@gmail.com>
2024-01-10 06:01:16 +0000

    move endpoints

--- orchestra/web/api/admin/views.py
+++ orchestra/web/api/admin/views.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import List
 
 from fastapi import APIRouter
 from fastapi.param_functions import Depends
@@ -14,6 +15,7 @@ from orchestra.db.dao.recharge_dao import RechargeDAO
 from orchestra.db.dao.recharge_type_dao import RechargeTypeDAO
 from orchestra.db.dao.task_dao import TaskDAO
 from orchestra.db.dao.users_dao import UsersDAO
+from orchestra.db.models.orchestra_models import Users
 from orchestra.web.api.admin.schema import (  # noqa: WPS235
     DatapointModelRequest,
     EndpointModelRequest,
@@ -25,9 +27,42 @@ from orchestra.web.api.admin.schema import (  # noqa: WPS235
     RechargeModelRequest,
     RechargeTypeModelRequest,
     TaskModelRequest,
+    UsersModelResponse,
 )
 
 router = APIRouter()
+
+
+@router.get("/get_all_users", response_model=List[UsersModelResponse])
+async def get_users_models(
+    limit: int = 10,
+    offset: int = 0,
+    users_dao: UsersDAO = Depends(),
+) -> List[Users]:
+    """
+    Retrieve all users objects from the database.
+
+    :param limit: limit of users objects, defaults to 10.
+    :param offset: offset of users objects, defaults to 0.
+    :param users_dao: DAO for users models.
+    :return: list of users objects from database.
+    """
+    return await users_dao.get_all_users(limit=limit, offset=offset)
+
+
+@router.get("/get_user", response_model=List[UsersModelResponse])
+async def get_user(
+    id: str,  # noqa: WPS125
+    users_dao: UsersDAO = Depends(),
+) -> List[Users]:
+    """
+    Retrieve specific users object from the database.
+
+    :param id: id of users instance.
+    :param users_dao: DAO for users models.
+    :return: list of users objects from database.
+    """
+    return await users_dao.filter(id=id)
 
 
 @router.put("/create_datapoint")
