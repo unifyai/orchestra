6e0bdf62c331ac3fd0dda6eec929a21b6e0f8562
kurshakuz <abilkasov@gmail.com>
2024-01-10 06:01:16 +0000

    move endpoints

--- orchestra/web/api/admin/schema.py
+++ orchestra/web/api/admin/schema.py
@@ -149,3 +149,16 @@ class TaskModelRequest(BaseModel):
 
     name: str
     modality: str
+
+
+class UsersModelResponse(BaseModel):
+    """
+    Response model for users models.
+
+    Attributes:
+        id (str): The id of the users.
+        credits (float): The credits of the users.
+    """
+
+    id: str
+    credits: float
