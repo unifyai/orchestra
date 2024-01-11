621ac3d49279e882a8b88bfafbf50df42029c9d9
aparajith21 <58738384+aparajith21@users.noreply.github.com>
2024-01-11 08:23:52 +0000

    move metric, modality, task to admin endpoint

--- orchestra/web/api/admin/views.py
+++ orchestra/web/api/admin/views.py
@@ -15,12 +15,15 @@ from orchestra.db.dao.recharge_dao import RechargeDAO
 from orchestra.db.dao.recharge_type_dao import RechargeTypeDAO
 from orchestra.db.dao.task_dao import TaskDAO
 from orchestra.db.dao.users_dao import UsersDAO
-from orchestra.db.models.orchestra_models import (
+from orchestra.db.models.orchestra_models import (  # noqa: WPS235
     Datapoint,
     Endpoint,
     License,
+    Metric,
+    Modality,
     Recharge,
     RechargeType,
+    Task,
     Users,
 )
 from orchestra.web.api.admin.schema import (  # noqa: WPS235
@@ -31,7 +34,9 @@ from orchestra.web.api.admin.schema import (  # noqa: WPS235
     LicenseModelRequest,
     LicenseModelResponse,
     MetricModelRequest,
+    MetricModelResponse,
     ModalityModelRequest,
+    ModalityModelResponse,
     ModelRequest,
     ProviderModelRequest,
     RechargeModelRequest,
@@ -39,6 +44,7 @@ from orchestra.web.api.admin.schema import (  # noqa: WPS235
     RechargeTypeModelRequest,
     RechargeTypeModelResponse,
     TaskModelRequest,
+    TaskModelResponse,
     UsersModelResponse,
 )
 
@@ -261,6 +267,102 @@ async def get_license(
     :return: list of license objects from database.
     """
     return await license_dao.filter(name=name)
+
+
+@router.get("/get_all_metrics", response_model=List[MetricModelResponse])
+async def get_metric_models(
+    limit: int = 10,
+    offset: int = 0,
+    metric_dao: MetricDAO = Depends(),
+) -> List[Metric]:
+    """
+    Retrieve all metric objects from the database.
+
+    :param limit: limit of metric objects, defaults to 10.
+    :param offset: offset of metric objects, defaults to 0.
+    :param metric_dao: DAO for metric models.
+    :return: list of metric objects from database.
+    """
+    return await metric_dao.get_all_metrics(limit=limit, offset=offset)
+
+
+@router.get("/get_metric", response_model=List[MetricModelResponse])
+async def get_metric(
+    name: str,
+    metric_dao: MetricDAO = Depends(),
+) -> List[Metric]:
+    """
+    Retrieve specific metric object from the database.
+
+    :param name: name of metric instance.
+    :param metric_dao: DAO for metric models.
+    :return: list of metric objects from database.
+    """
+    return await metric_dao.filter(name=name)
+
+
+@router.get("/get_all_modalities", response_model=List[ModalityModelResponse])
+async def get_modality_models(
+    limit: int = 10,
+    offset: int = 0,
+    modality_dao: ModalityDAO = Depends(),
+) -> List[Modality]:
+    """
+    Retrieve all modality objects from the database.
+
+    :param limit: limit of modality objects, defaults to 10.
+    :param offset: offset of modality objects, defaults to 0.
+    :param modality_dao: DAO for modality models.
+    :return: list of modality objects from database.
+    """
+    return await modality_dao.get_all_modalities(limit=limit, offset=offset)
+
+
+@router.get("/get_modality", response_model=List[ModalityModelResponse])
+async def get_modality(
+    name: str,
+    modality_dao: ModalityDAO = Depends(),
+) -> List[Modality]:
+    """
+    Retrieve specific modality object from the database.
+
+    :param name: name of modality object.
+    :param modality_dao: DAO for modality models.
+    :return: modality object from database.
+    """
+    return await modality_dao.filter(name=name)
+
+
+@router.get("/get_all_tasks", response_model=List[TaskModelResponse])
+async def get_task_models(
+    limit: int = 10,
+    offset: int = 0,
+    task_dao: TaskDAO = Depends(),
+) -> List[Task]:
+    """
+    Retrieve all task objects from the database.
+
+    :param limit: limit of task objects, defaults to 10.
+    :param offset: offset of task objects, defaults to 0.
+    :param task_dao: DAO for task models.
+    :return: list of task objects from database.
+    """
+    return await task_dao.get_all_tasks(limit=limit, offset=offset)
+
+
+@router.get("/get_task", response_model=List[TaskModelResponse])
+async def get_task(
+    name: str,
+    task_dao: TaskDAO = Depends(),
+) -> List[Task]:
+    """
+    Retrieve specific task object from the database.
+
+    :param name: name of task object.
+    :param task_dao: DAO for task models.
+    :return: task object from database.
+    """
+    return await task_dao.filter(name=name)
 
 
 @router.put("/create_datapoint")
--- orchestra/web/api/modality/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Modality model API."""
-from orchestra.web.api.modality.views import router
-
-__all__ = ["router"]
