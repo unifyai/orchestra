dcf9ddcc69b272195e8f602d492cf54d9dac4d7f
Rishab Mallick <rishabmallick6@gmail.com>
2024-01-15 16:55:15 +0000

    refactor: removes all error raises

--- benchmark/perf.py
+++ benchmark/perf.py
@@ -55,7 +55,6 @@ def evaluate_answers(
     :param query: The problem sent to models being benchmarked.
     :param ground_truth: The correct answer for the query.
     :param answer: The answer to be evaluated.
-    :raises ValueError: If evaluator_model provider throws error during API call.
     :return: The score of the evaluated answer.
     """
     system = (
@@ -83,7 +82,8 @@ def evaluate_answers(
         max_tokens=MAX_TOKENS,
     )
     if evaluator_result is None:
-        raise ValueError(f"{evaluator_model} on {provider} threw an error during call")
+        logger.error(f"{evaluator_model} on {provider} threw an error during call")
+        return 0
     found = re.search(
         r"Score: (\d+)",
         evaluator_result[0].choices[0].message.content,  # noqa: WPS219, E501
@@ -212,7 +212,7 @@ def add_cost_info(  # noqa: WPS211
     elif cost_data.get("per_second"):
         logger.info(
             f"Per second pricing not supported yet so skipped "
-            f"{model_name}: {provider_name} ",
+            f"{model_name} on {provider_name} ",
         )
     else:
         model_results[model_name][provider_name]["input_cost_llm"] = cost_data["prompt"]
@@ -221,7 +221,9 @@ def add_cost_info(  # noqa: WPS211
         ]
 
 
-def get_evaluator_provider(evaluator: str) -> BaseCompletionProvider:  # noqa: D103
+def get_evaluator_provider(  # noqa: D103
+    evaluator: str,
+) -> Optional[BaseCompletionProvider]:
     for provider_name, provider_class in PROVIDER_CLASSES.items():
         if evaluator in provider_class.supported_models:
             evaluator_provider = provider_class()
@@ -229,7 +231,8 @@ def get_evaluator_provider(evaluator: str) -> BaseCompletionProvider:  # noqa: D
                 api_key=str(os.getenv(f"ORCHESTRA_{provider_name.upper()}_API_KEY")),
             )
             return evaluator_provider
-    raise ValueError("Evaluator model not supported")
+    logger.error("Evaluator model not supported so skipped student-teacher scoring")
+    return None
 
 
 def calculate_results(  # noqa: D103
@@ -380,7 +383,7 @@ def run_benchmark(  # noqa: C901, WPS210, WPS220, WPS231
     benchmark_problems_path: str = "benchmark/problems.jsonl",
     evaluator: Optional[str] = None,
     print_table: bool = False,
-) -> Dict:
+) -> Optional[Dict]:
     """
     Benchmarks selected language models across diverse cloud providers.
 
@@ -393,11 +396,11 @@ def run_benchmark(  # noqa: C901, WPS210, WPS220, WPS231
     :param benchmark_problems_path: JSONL file path with problems.
     :param evaluator: Model used to evaluate the answers. Should be SOTA like gpt-4.
     :param print_table: Whether to print the table or not.
-    :raises ValueError: If no models are provided to benchmark.
     :return: Dict with benchmarking results of model on providers.
     """
     if not models:
-        raise ValueError("No models provided to benchmark")
+        logger.error("No models provided to benchmark")
+        return None
     problems = load_problems(benchmark_problems_path)
 
     model_results: Dict[str, Any] = {}
@@ -405,6 +408,8 @@ def run_benchmark(  # noqa: C901, WPS210, WPS220, WPS231
     logger.info("Currently benchmarking: ")
     for provider_name, provider_class in PROVIDER_CLASSES.items():
         for model_name in models:
+            # keep this try except block since it helps catch edge case
+            # errors inside imported functions
             try:
                 benchmark_model(
                     model_name,
@@ -421,14 +426,15 @@ def run_benchmark(  # noqa: C901, WPS210, WPS220, WPS231
         logger.info("--------------------")
     if evaluator:
         evaluator_provider = get_evaluator_provider(evaluator)
-        for provider_results in model_results.values():
-            for provider_data in provider_results.values():
-                provider_data["score"] = calculate_score(
-                    evaluator,
-                    evaluator_provider,
-                    provider_data,
-                    problems,
-                )
+        if evaluator_provider:
+            for provider_results in model_results.values():
+                for provider_data in provider_results.values():
+                    provider_data["score"] = calculate_score(
+                        evaluator,
+                        evaluator_provider,
+                        provider_data,
+                        problems,
+                    )
     if print_table:
         table = create_table(model_results, evaluator)
         print(table)  # noqa: WPS421
@@ -578,21 +584,24 @@ if __name__ == "__main__":
     ]
     model_list = list(set(model_list))
     benchmarking_results = run_benchmark(model_list, print_table=False)
-    logger.info("Pushing metrics to DB")
-    metrics_to_push = [
-        "output_toks_per_sec",
-        "context_window",
-        "cold_start_latency",
-        # "cold_start_latency_std",
-        "input_cost_llm",
-        "output_cost_llm",
-        "input_cost_llm_per_character",
-        "output_cost_llm_per_character",
-    ]
-    asyncio.run(
-        process_benchmarking_results(
-            benchmarking_results,
-            metrics_to_push,
-        ),
-    )
-    logger.info("All done. Closing job.")
+    if benchmarking_results:
+        logger.info("Pushing metrics to DB")
+        metrics_to_push = [
+            "output_toks_per_sec",
+            "context_window",
+            "cold_start_latency",
+            # "cold_start_latency_std",
+            "input_cost_llm",
+            "output_cost_llm",
+            "input_cost_llm_per_character",
+            "output_cost_llm_per_character",
+        ]
+        asyncio.run(
+            process_benchmarking_results(
+                benchmarking_results,
+                metrics_to_push,
+            ),
+        )
+        logger.info("All done. Closing job.")
+    else:
+        logger.error("Closing job since benchmarking results are empty")
