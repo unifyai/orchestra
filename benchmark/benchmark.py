c8ed49f3a3621ebe5e62ae5e42ea23046286b66b
guillesanbri <Guillesanbri@hotmail.com>
2024-01-22 22:06:36 +0000

    docs + start cleaning linting

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -14,17 +14,20 @@ from sqlalchemy.orm import sessionmaker
 from orchestra.db.models.orchestra_models import Endpoint, Model, Provider
 
 
-def read_configs():
-    """
-    Reads config file and returns a list of dictionaries.
-    """
+def read_configs(config_file):
+    """Reads config file and returns a list of dictionaries."""
     # Returns a list of dictionaries
-    raise NotImplementedError
+    return
 
 
-def get_db_session():  # noqa: WPS210
-    """
-    Initializes an async db engine
+def create_db_session() -> AsyncSession:  # noqa: WPS210
+    """Creates an async db session.
+
+    If ORCHESTRA_<> env vars are not defined, it defaults to
+    orchestra:orchestra@localhost:5432/orchestra.
+
+    Returns:
+        AsyncSession: Async SQLAlchemy session.
     """
     user = os.getenv("ORCHESTRA_DB_USER", "orchestra")
     password = os.getenv("ORCHESTRA_DB_PASS", "orchestra")
@@ -73,7 +76,7 @@ async def worker_loop(input_queue, output_queue, done_event, configs):
         if endpoint is None:
             break
 
-        # Retrieve/fabricate the callable based on the model name and the provider name
+        # Retrieve/fabricate a callable based on the model the provider
         endpoint_fn = None  # TODO
 
         # Initialise the benchmark runner(s)
@@ -86,9 +89,11 @@ async def worker_loop(input_queue, output_queue, done_event, configs):
         #     # Run the benchmark
         #     result = runner()
         #     # Store the result in the db
-        #     # TODO: This should actually put the results into a queue that gets consumed by the db task
-        #     # we will need a new table which stores each run, with its regime (concurrency or QPS) and region
-        #     # the datapoints will then have a runID pointing to that run and will store the actual value
+        #     # TODO: This should actually put the results into a queue that
+        #     # gets consumed by the db task we will need a new table which
+        #     # stores each run, with its regime (concurrency or QPS) and region
+        #     # the datapoints will then have a runID pointing to that run and
+        #     # will store the actual value
         #     # store_datapoint(db_engine, region, result)
         #     # Log results
         #     logging.info(repr(runner))
@@ -128,7 +133,7 @@ async def retrieve_all_endpoints(async_session: AsyncSession) -> List[Dict]:
                 "id": result.Endpoint.id,
                 "provider": result.Provider.name,
                 "model": result.Model.mdl_code,
-            }
+            },
         )
     return endpoints
 
@@ -137,28 +142,27 @@ def store_datapoint():
     raise NotImplementedError
 
 
-async def main():
+async def main():  # noqa: WPS210
     # Define config file to use
     CONFIG_FILE = "production_benchmark.config"  # json/yaml
     # Read the config file where all the configurations are defined
     # This includes the load testing parameters, the number of inputs, length of the inputs, and length of the outputs
-    configs = {}  # read_configs(CONFIG_FILE)
+    configs = read_configs(CONFIG_FILE)
 
     # Get region information from the GCP instance
     region = os.getenv("BENCHMARK_REGION")
     if not region:
-        pass
-        # raise ValueError("Region ENV VAR was not declared")
+        raise ValueError("BENCHMARK_REGION env was not declared")
 
     # Initialise db engine
-    async_db_session = get_db_session()
+    async_db_session = create_db_session()
 
     # Get list of endpoints in our db
     endpoints = await retrieve_all_endpoints(async_db_session)
 
     # Configure concurrent workers and tasks
     num_workers = int(os.getenv("BENCHMARK_NUM_WORKERS", "5"))
-    db_commit_period = int(os.getenv("BENCHMARK_DB_COMMIT_PERIOD", "60"))
+    db_commit_period = int(os.getenv("BENCHMARK_DB_COMMIT_PERIOD", "10"))
     # Create an asyncio.Queue for inputs (endpoints) and outputs (datapoints)
     input_queue, output_queue = asyncio.Queue(), asyncio.Queue()
     # Create an event to signal the completion of worker tasks
