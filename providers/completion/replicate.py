8df258a19282977bdaf5b1ca02a273e0e1741145
aparajith21 <58738384+aparajith21@users.noreply.github.com>
2024-01-22 16:13:28 +0530

    chore: modifying response_ms to _response_ms in replicate (#69)
    * modifying response_ms to _response_ms in replicate
    
    * _response_ms in ms

--- providers/completion/replicate.py
+++ providers/completion/replicate.py
@@ -320,7 +320,7 @@ class Replicate(BaseCompletionProvider):
             id=f"chatcmpl-{str(uuid.uuid4())}",  # noqa: WPS237
             choices=[Choices(message=Message(), index=0, finish_reason="stop")],
             created=created,
-            response_ms=predict_time,
+            _response_ms=predict_time * 1000,
             usage=usage,
             object="chat.completion",
         )
