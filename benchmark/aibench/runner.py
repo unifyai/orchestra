7db332d5261b8be857785eb92a5523757ce412ba
guillesanbri <Guillesanbri@hotmail.com>
2024-01-24 17:19:22 +0000

    add utils to the runner

--- benchmark/aibench/runner.py
+++ benchmark/aibench/runner.py
@@ -6,31 +6,42 @@ class AIBenchRunner:
         self.input_policy = input_policy  # short | long | mixed
 
         # Computed metrics
+        # TODO: These need to be aligned between them
         self.ttft = list()
         self.end_to_end_latency = list()
-        self.itl = list()
-        self.cold_start = list()
+        self.cold_start = 0
         self.output_tokens = list()
         self.failed_queries = 0
 
     @property
+    def itl(self):
+        return [
+            (e2e_lat - ttft) / (o_tks - 1)
+            for e2e_lat, ttft, o_tks in zip(
+                self.end_to_end_latency, self.ttft, self.output_tokens
+            )
+        ]
+
+    @property
     def output_tks_per_sec(self):
-        raise NotImplementedError
+        return [1 / itl for itl in self.itl]
 
     def __repr__(self):
         # developer facing print (for logging)
         raise NotImplementedError
 
-    def concurrent_requests(self):
-        raise NotImplementedError
-
-    def QPS(self):
-        raise NotImplementedError
-
-    async def say_after(self, delay, what):
-        print(time.time())
-        await asyncio.sleep(delay)
-        print(what)
+    def as_dict(self):
+        return {
+            "test_load": self.test_load,
+            "input_policy": self.input_policy,
+            "ttft": self.ttft,
+            "e2e_latency": self.end_to_end_latency,
+            "itl": self.itl,
+            "cold_start": self.cold_start,
+            "output_tokens": self.output_tokens,
+            "output_tks_per_sec": self.output_tks_per_sec,
+            "failed_queries": self.failed_queries,
+        }
 
     async def __call__(self):
         concurrent_requests = []
