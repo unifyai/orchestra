1d43d4fe43f531f48f458eca94618a6ab0204872
kurshakuz <abilkasov@gmail.com>
2024-01-25 17:41:45 +0100

    Add measured_at and units columns (#78)

--- orchestra/db/dao/datapoint_dao.py
+++ orchestra/db/dao/datapoint_dao.py
@@ -18,28 +18,28 @@ class DatapointDAO:
     async def create_datapoint(  # noqa: WPS211
         self,
         benchmark_run_id: int,
-        measured_at: datetime.datetime,
         metric_name: str,
         value: float,
         tooltip: str,
+        measured_at: datetime.datetime,
     ) -> None:
         """
         Add single datapoint to session.
 
         :param benchmark_run_id: benchmark_run_id of a datapoint.
-        :param measured_at: measured_at of a datapoint.
         :param metric_name: metric_name of a datapoint.
         :param value: value of a datapoint.
         :param tooltip: tooltip of a datapoint.
+        :param measured_at: measured_at of a datapoint.
 
         """
         self.session.add(
             Datapoint(
                 benchmark_run_id=benchmark_run_id,
-                measured_at=measured_at,
                 metric_name=metric_name,
                 value=value,
                 tooltip=tooltip,
+                measured_at=measured_at,
             ),
         )
 
@@ -57,22 +57,24 @@ class DatapointDAO:
 
         return list(raw_datapoints.scalars().fetchall())
 
-    async def filter(  # noqa: WPS211
+    async def filter(  # noqa: WPS211, C901
         self,
         id: Optional[int] = None,  # noqa: WPS125
         benchmark_run_id: Optional[int] = None,
-        measured_at: Optional[datetime.datetime] = None,
         metric_name: Optional[str] = None,
         value: Optional[float] = None,
+        tooltip: Optional[str] = None,
+        measured_at: Optional[datetime.datetime] = None,
     ) -> List[Datapoint]:
         """
         Get specific datapoint model.
 
         :param id: id of datapoint instance.
         :param benchmark_run_id: benchmark_run_id of datapoint instance.
-        :param measured_at: measured_at of datapoint instance.
         :param metric_name: metric_name of datapoint instance.
         :param value: value of datapoint instance.
+        :param tooltip: tooltip of datapoint instance.
+        :param measured_at: measured_at of datapoint instance.
         :return: datapoint models.
         """
         query = select(Datapoint)
@@ -80,11 +82,13 @@ class DatapointDAO:
             query = query.where(Datapoint.id == id)
         if benchmark_run_id:
             query = query.where(Datapoint.benchmark_run_id == benchmark_run_id)
-        if measured_at:
-            query = query.where(Datapoint.measured_at == measured_at)
         if metric_name:
             query = query.where(Datapoint.metric_name == metric_name)
         if value:
             query = query.where(Datapoint.value == value)
+        if tooltip:
+            query = query.where(Datapoint.tooltip == tooltip)
+        if measured_at:
+            query = query.where(Datapoint.measured_at == measured_at)
         rows = await self.session.execute(query)
         return list(rows.scalars().fetchall())
