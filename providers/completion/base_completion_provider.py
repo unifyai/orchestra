a380480f31d639992c55d6282257f0db2f381e59
aparajith21 <58738384+aparajith21@users.noreply.github.com>
2024-02-01 19:09:08 +0000

    saving the reelase hopefully

--- providers/completion/base_completion_provider.py
+++ providers/completion/base_completion_provider.py
@@ -6,7 +6,7 @@ import litellm
 import openai
 import tiktoken
 from litellm.utils import ModelResponse, Usage
-
+import asyncio 
 logger = logging.getLogger(__name__)
 
 # Pricing info of providers with pay-per-token model is
@@ -129,7 +129,7 @@ class BaseCompletionProvider:
             if stream:
                 return (
                     AsyncGeneratorWrapper(
-                        litellm.completion(
+                        litellm.acompletion(
                             model=provider_model_endpoint,
                             messages=messages,
                             max_tokens=max_tokens,
@@ -184,7 +184,7 @@ class AsyncGeneratorWrapper:  # noqa: D101
         whole = ""
         usage = {}
         try:  # noqa: WPS501
-            for part in self._response:
+            async for part in await self._response:
                 usage = part.get("usage", {})
 
                 choices = [
