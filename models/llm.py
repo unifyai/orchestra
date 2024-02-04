723a2e1146d0cf474015a48fa91a18d72f09252c
aparajith21 <58738384+aparajith21@users.noreply.github.com>
2024-02-04 00:05:21 +0000

    setting api key before a request is made

--- models/llm.py
+++ models/llm.py
@@ -18,10 +18,13 @@ class CompletionsModel:
                 f"Model {model} not supported by {provider}",
             )
 
+        self.provider = provider
         self.provider_obj = PROVIDER_CLASSES[provider]()
         self.model = model.lower()
 
-        if provider == "vertex-ai":
+
+    def set_credentials(self):
+        if self.provider == "vertex-ai":
             from providers.completion.vertexai import VertexAI  # noqa: WPS433
 
             self.provider_obj = cast(VertexAI, self.provider_obj)
@@ -37,7 +40,7 @@ class CompletionsModel:
             self.provider_obj.set_api_key(
                 api_key=str(
                     os.getenv(
-                        f"ORCHESTRA_{provider.replace('-', '_').upper()}_API_KEY",  # noqa: WPS237, E501
+                        f"ORCHESTRA_{self.provider.replace('-', '_').upper()}_API_KEY",  # noqa: WPS237, E501
                     ),
                 ),
             )
@@ -52,7 +55,7 @@ class CompletionsModel:
         temperature: float = 0.9,
         stream: bool = False,
     ) -> Union[ModelResponse, Any]:
-
+        self.set_credentials()
         return self.provider_obj.complete(
             self.model,
             messages,
