98f3b278739ff3957365d155f22dbd678139872b
aparajith21 <58738384+aparajith21@users.noreply.github.com>
2024-02-06 15:47:17 +0000

    fetching endpoint id wiht inference enpdoint

--- orchestra/web/api/inference/views.py
+++ orchestra/web/api/inference/views.py
@@ -11,10 +11,16 @@ from litellm.utils import Usage
 from models.imagegen import ImagegenModel
 from models.llm import CompletionsModel
 
+from orchestra.db.dao.endpoint_dao import EndpointDAO
+from orchestra.db.dao.model_dao import ModelDAO
+from orchestra.db.dao.provider_dao import ProviderDAO
 from orchestra.db.dao.query_dao import QueryDAO
 from orchestra.db.dao.users_dao import UsersDAO
 from orchestra.web.api.chat_completion.schema import ChatCompletionResponse
+from orchestra.web.api.endpoint.views import get_endpoint
 from orchestra.web.api.inference.schema import InferenceRequest, InferenceResponse
+from orchestra.web.api.model.views import get_model
+from orchestra.web.api.provider.views import get_provider
 from orchestra.web.api.query.schema import QueryModelRequest
 from orchestra.web.api.query.views import create_query_model
 from orchestra.web.api.users.views import get_credits
@@ -35,10 +41,13 @@ def get_model_type(model_name):  # noqa: D103
 
 
 @router.post("/inference", response_model=InferenceResponse)
-async def get_inference(  # noqa: C901, WPS212, WPS210, WPS231, E501
+async def get_inference(  # noqa: C901, WPS212, WPS210, WPS231, E501, WPS211, WPS217
     request_fastapi: Request,
     request: InferenceRequest,
     users_dao: UsersDAO = Depends(),
+    model_dao: ModelDAO = Depends(),
+    provider_dao: ProviderDAO = Depends(),
+    endpoint_dao: EndpointDAO = Depends(),
     query_dao: QueryDAO = Depends(),
 ) -> Union[InferenceResponse, StreamingResponse]:
     """
@@ -47,6 +56,11 @@ async def get_inference(  # noqa: C901, WPS212, WPS210, WPS231, E501
     :param request_fastapi: FastAPI request object.
     :param request: InferenceRequest object.
     :param users_dao: DAO for users models.
+    :param model_dao: DAO for model models.
+    :param provider_dao: DAO for provider models.
+    :param endpoint_dao: DAO for endpoint models.
+    :param query_dao: DAO for query models.
+
     :return: Model-specific InferenceResponse object.
 
     :raises HTTPException: when user has insufficient credits.
@@ -57,14 +71,46 @@ async def get_inference(  # noqa: C901, WPS212, WPS210, WPS231, E501
     user_id = request_fastapi.state.user_id
     user = await get_credits(request_fastapi, users_dao=users_dao)
     available_credits = float(user.credits if user else 0)
+    model = request.model
+    provider = request.provider
+    try:
+        model_id = int((await get_model(mdl_code=model, model_dao=model_dao))[0].id)
+    except Exception:
+        raise HTTPException(
+            status_code=422,  # noqa: WPS432
+            detail="Model not found",
+        )
+    try:
+        provider_id = int(
+            (await get_provider(name=provider, provider_dao=provider_dao))[0].id,
+        )
+    except Exception:
+        raise HTTPException(
+            status_code=422,  # noqa: WPS432
+            detail="Provider not found",
+        )
 
-    model_type = get_model_type(request.model)
+    endpoint_ids = await get_endpoint(
+        mdl_id=model_id,
+        provider_id=provider_id,
+        endpoint_dao=endpoint_dao,
+        model_dao=model_dao,
+        provider_dao=provider_dao,
+    )
+    endpoint_id = next(
+        (
+            int(endpoint.endpoint_id)
+            for endpoint in endpoint_ids
+            if endpoint.provider_id == provider_id
+        ),
+        None,
+    )
+
+    model_type = get_model_type(model)
     if model_type == "chat":
-        provider = request.provider
-        model = request.model
         language_model = CompletionsModel(
-            provider=request.provider,
-            model=request.model,
+            provider=provider,
+            model=model,
         )
 
         cost_max = language_model.get_cost_max()
@@ -148,8 +194,8 @@ async def get_inference(  # noqa: C901, WPS212, WPS210, WPS231, E501
         )
     elif model_type == "image":
         image_model = ImagegenModel(
-            provider=request.provider,
-            model=request.model,
+            provider=provider,
+            model=model,
         )
         kwargs = {
             "init_image": request.arguments.get("init_image", None),
@@ -176,7 +222,7 @@ async def get_inference(  # noqa: C901, WPS212, WPS210, WPS231, E501
             # TODO: Handle when response is None
             return InferenceResponse(
                 response={
-                    "model": request.model,
+                    "model": model,
                     "created": 0,
                     "images": [],
                     "object": "image.generations",
@@ -189,7 +235,7 @@ async def get_inference(  # noqa: C901, WPS212, WPS210, WPS231, E501
         ]
         return InferenceResponse(
             response={
-                "model": request.model,
+                "model": model,
                 "created": response.get("created", None),
                 "images": base64_images,
                 "object": "image.generation",
