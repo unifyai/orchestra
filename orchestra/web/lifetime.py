fc3f40f43fc133b1ed0881d5f8df4efb6855b1d1
aparajith21 <58738384+aparajith21@users.noreply.github.com>
2024-02-09 14:13:32 -0500

    increasing pool size, overflow of sqlalchemy (#124)
    * allowing unlimited overflow to sqlalchemy pool
    
    * setting pool size to 25 and max_overflow to 50

--- orchestra/web/lifetime.py
+++ orchestra/web/lifetime.py
@@ -31,7 +31,9 @@ def _setup_db(app: FastAPI) -> None:  # pragma: no cover
 
     :param app: fastAPI application.
     """
-    engine = create_async_engine(str(settings.db_url), echo=settings.db_echo)
+    engine = create_async_engine(
+        str(settings.db_url), echo=settings.db_echo, pool_size=25, max_overflow=50
+    )
     session_factory = async_sessionmaker(
         engine,
         expire_on_commit=False,
