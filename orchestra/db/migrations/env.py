372c2cd262534c7da14493f33efb4803be3834e0
aparajith21 <58738384+aparajith21@users.noreply.github.com>
2024-02-15 16:17:37 +0000

    fix migrations v2

--- orchestra/db/migrations/env.py
+++ orchestra/db/migrations/env.py
@@ -67,7 +67,7 @@ def do_run_migrations(connection: Connection) -> None:
         context.run_migrations()
 
 
-async def run_migrations_online() -> None:
+def run_migrations_online() -> None:
     """
     Run migrations in 'online' mode.
 
