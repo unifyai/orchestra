f2aaec0a08fb57c5572f08e3a51af7e884d35fc3
guillesanbri <Guillesanbri@hotmail.com>
2024-02-28 18:54:34 +0100

    Better errors (#160) (#161)
    * add digest to SQL errors
    
    * change error response when db-connector errors out

--- orchestra/web/api/dependencies.py
+++ orchestra/web/api/dependencies.py
@@ -1,3 +1,5 @@
+import hashlib
+import logging
 import os
 
 from fastapi import Depends, HTTPException, Request
@@ -7,6 +9,7 @@ from requests import request  # type: ignore
 from orchestra.settings import settings
 
 security = HTTPBearer()
+logger = logging.getLogger(__name__)
 
 
 def auth_api_key(
@@ -27,10 +30,20 @@ def auth_api_key(
         headers={},
     )
 
-    if auth_ret.status_code != 200:  # noqa: WPS432
+    # TODO: This may be missleading and should have a different http code
+    # (db-connector issue)
+    # TODO: Move this to utils file with other shared HTTP responses.
+    if auth_ret.status_code == 404:
         raise HTTPException(
             status_code=401,  # noqa: WPS432
             detail="Invalid API key. You can generate one at https://console.unify.ai/login",
+        )
+    elif auth_ret.status_code != 200:  # noqa: WPS432
+        digest = hashlib.shake_256(auth_ret.text.encode()).digest(4).hex()
+        logger.error(f"Digest {digest}: {auth_ret.text}")
+        raise HTTPException(
+            status_code=500,  # noqa: WPS432
+            detail=f"Internal Server Error. Digest: {digest}",
         )
     request_fastapi.state.user_id = auth_ret.json()["user_id"]
 
