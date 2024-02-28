a4c002d68e162ab9e96c75bbe6723841528383a6
guillesanbri <Guillesanbri@hotmail.com>
2024-02-28 13:44:58 +0000

    add digest to SQL errors

--- orchestra/db/dependencies.py
+++ orchestra/db/dependencies.py
@@ -1,7 +1,12 @@
+import hashlib
+import logging
 from typing import Generator
 
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from starlette.requests import Request
+
+logger = logging.getLogger(__name__)
 
 
 def get_db_session(request: Request) -> Generator[Session, None, None]:
@@ -15,6 +20,13 @@ def get_db_session(request: Request) -> Generator[Session, None, None]:
 
     try:  # noqa: WPS501
         yield session
+    except Exception as e:
+        digest = hashlib.shake_256(str(e).encode()).digest(4).hex()
+        logger.error(f"Digest {digest}: {e}")
+        raise HTTPException(
+            status_code=500,  # noqa: WPS432
+            detail=f"Internal Server Error. Digest: {digest}",
+        )
     finally:
         session.commit()
         session.close()
