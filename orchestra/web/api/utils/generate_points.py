ed3098e9ac09039d99cdacf18d3ff4967a88002b
guillesanbri <Guillesanbri@hotmail.com>
2024-05-01 21:20:28 +0000

    formatting

--- orchestra/web/api/utils/generate_points.py
+++ orchestra/web/api/utils/generate_points.py
@@ -19,25 +19,26 @@ def reorganize_data(raw_responses):
                 datasets[response["dataset_name"]].append(relevant_info)
             else:
                 datasets[response["dataset_name"]] = [relevant_info]
-    
+
     final_results = dict()
     for dataset in datasets:
         prompts = dict()
         for benchmark in datasets[dataset]:
             if benchmark["prompt"] in prompts:
-                prompts[benchmark["prompt"]][f"{benchmark['mdl_name']}_score"] = benchmark["score"]
-                prompts[benchmark["prompt"]][f"{benchmark['mdl_name']}_pred"] = benchmark["pred"]
+                prompts[benchmark["prompt"]][f"{benchmark['mdl_name']}_score"] = (
+                    benchmark["score"]
+                )
+                prompts[benchmark["prompt"]][f"{benchmark['mdl_name']}_pred"] = (
+                    benchmark["pred"]
+                )
             else:
                 prompts[benchmark["prompt"]] = {
                     f"{benchmark['mdl_name']}_score": benchmark["score"],
-                    f"{benchmark['mdl_name']}_pred": benchmark["pred"]
+                    f"{benchmark['mdl_name']}_pred": benchmark["pred"],
                 }
         dataset_results = []
         for prompt in prompts:
-            dataset_results.append({
-                "prompt": prompt,
-                **prompts[prompt]
-            })
+            dataset_results.append({"prompt": prompt, **prompts[prompt]})
         final_results[dataset] = dataset_results
 
     return final_results
@@ -55,50 +56,74 @@ def generate_router_points(data):
     endpoint_costs = {endpoint: metrics[endpoint]["cost"] for endpoint in endpoints}
     endpoint_ttft = {endpoint: metrics[endpoint]["ttft"] for endpoint in endpoints}
     endpoint_itl = {endpoint: metrics[endpoint]["itl"] for endpoint in endpoints}
-    
-    def get_mean(endpoint): 
+
+    def get_mean(endpoint):
         scores = [
-            prompt[endpoint_to_model(endpoint)] 
-            for prompt in prompt_list 
+            prompt[endpoint_to_model(endpoint)]
+            for prompt in prompt_list
             if endpoint_to_model(endpoint) in prompt
         ]
         if len(scores):
             return mean(scores)
         return None
-    
+
     for dataset in data:
         prompt_list = data[dataset]
+        endpoint_scores = {endpoint: get_mean(endpoint) for endpoint in endpoints}
         endpoint_scores = {
-            endpoint: get_mean(endpoint)
-            for endpoint in endpoints
-        }
-        endpoint_scores = {
-            endpoint: score for endpoint, score in endpoint_scores.items()
+            endpoint: score
+            for endpoint, score in endpoint_scores.items()
             if score != None
         }
         current_endpoints = list(endpoint_scores.keys())
         if not current_endpoints:
             final_scores[dataset] = []
             continue
-        
-        cost_multiplier = mean([endpoint_scores[endpoint] / endpoint_costs[endpoint]  for endpoint in current_endpoints])
-        ttft_multiplier = mean([endpoint_scores[endpoint] / endpoint_ttft[endpoint] for endpoint in current_endpoints])
-        itl_multiplier = mean([endpoint_scores[endpoint] / endpoint_itl[endpoint] for endpoint in current_endpoints])
-        
+
+        cost_multiplier = mean(
+            [
+                endpoint_scores[endpoint] / endpoint_costs[endpoint]
+                for endpoint in current_endpoints
+            ]
+        )
+        ttft_multiplier = mean(
+            [
+                endpoint_scores[endpoint] / endpoint_ttft[endpoint]
+                for endpoint in current_endpoints
+            ]
+        )
+        itl_multiplier = mean(
+            [
+                endpoint_scores[endpoint] / endpoint_itl[endpoint]
+                for endpoint in current_endpoints
+            ]
+        )
+
         num = 20
         a_values, b_values, c_values = [], [], []
-        a_start, b_start, c_start = math.log10(cost_multiplier * 3), math.log10(ttft_multiplier * 3), math.log10(itl_multiplier * 3)
+        a_start, b_start, c_start = (
+            math.log10(cost_multiplier * 3),
+            math.log10(ttft_multiplier * 3),
+            math.log10(itl_multiplier * 3),
+        )
         end = math.log10(1e-8)
-        a_step, b_step, c_step = (end - a_start) / (num - 1), (end - b_start) / (num - 1), (end - b_start) / (num - 1)
-        values = [(
-            10 ** (a_start + a_step * i), 
-            10 ** (b_start + b_step * i), 
-            10 ** (c_start + c_step * i)
-        ) for i in range(num)]
+        a_step, b_step, c_step = (
+            (end - a_start) / (num - 1),
+            (end - b_start) / (num - 1),
+            (end - b_start) / (num - 1),
+        )
+        values = [
+            (
+                10 ** (a_start + a_step * i),
+                10 ** (b_start + b_step * i),
+                10 ** (c_start + c_step * i),
+            )
+            for i in range(num)
+        ]
         a_values = [value[0] for value in values]
         b_values = [value[1] for value in values]
         c_values = [value[2] for value in values]
-        
+
         router_points = []
         for a_idx, a in enumerate(a_values):
             for b_idx, b in enumerate(b_values):
@@ -113,10 +138,10 @@ def generate_router_points(data):
                             model = endpoint_to_model(endpoint)
                             if model in prompt_list[i]:
                                 endpoint_scores[endpoint] = (
-                                    prompt_list[i][model] - 
-                                    a * endpoint_costs[endpoint] - 
-                                    b * endpoint_ttft[endpoint] - 
-                                    c * endpoint_itl[endpoint]
+                                    prompt_list[i][model]
+                                    - a * endpoint_costs[endpoint]
+                                    - b * endpoint_ttft[endpoint]
+                                    - c * endpoint_itl[endpoint]
                                 )
                         prompt_scores.append(endpoint_scores)
 
@@ -128,10 +153,11 @@ def generate_router_points(data):
                                 max_endpoint, max_objective = endpoint, objective
                         prompt_max.append(
                             {
-                                "endpoint": max_endpoint, 
+                                "endpoint": max_endpoint,
                                 "score": prompt_list[i][
-                                    endpoint_to_model(max_endpoint).replace("_pred", "") + "_score"
-                                ]
+                                    endpoint_to_model(max_endpoint).replace("_pred", "")
+                                    + "_score"
+                                ],
                             }
                         )
 
@@ -144,45 +170,107 @@ def generate_router_points(data):
                         if prompt["endpoint"] not in router_counts:
                             router_counts[prompt["endpoint"]] = 1
                             router_scores[prompt["endpoint"]] = prompt["score"]
-                            router_cost[prompt["endpoint"]] = endpoint_costs[prompt["endpoint"]]
-                            router_ttft[prompt["endpoint"]] = endpoint_ttft[prompt["endpoint"]]
-                            router_itl[prompt["endpoint"]] = endpoint_itl[prompt["endpoint"]]
+                            router_cost[prompt["endpoint"]] = endpoint_costs[
+                                prompt["endpoint"]
+                            ]
+                            router_ttft[prompt["endpoint"]] = endpoint_ttft[
+                                prompt["endpoint"]
+                            ]
+                            router_itl[prompt["endpoint"]] = endpoint_itl[
+                                prompt["endpoint"]
+                            ]
                         else:
                             router_counts[prompt["endpoint"]] += 1
                             router_scores[prompt["endpoint"]] += prompt["score"]
-                            router_cost[prompt["endpoint"]] += endpoint_costs[prompt["endpoint"]]
-                            router_ttft[prompt["endpoint"]] += endpoint_ttft[prompt["endpoint"]]
-                            router_itl[prompt["endpoint"]] += endpoint_itl[prompt["endpoint"]]
+                            router_cost[prompt["endpoint"]] += endpoint_costs[
+                                prompt["endpoint"]
+                            ]
+                            router_ttft[prompt["endpoint"]] += endpoint_ttft[
+                                prompt["endpoint"]
+                            ]
+                            router_itl[prompt["endpoint"]] += endpoint_itl[
+                                prompt["endpoint"]
+                            ]
 
-                    router_scores = {endpoint: router_scores[endpoint] / router_counts[endpoint] for endpoint in router_scores}
-                    router_cost = {endpoint: router_cost[endpoint] / router_counts[endpoint] for endpoint in router_cost}
-                    router_ttft = {endpoint: router_ttft[endpoint] / router_counts[endpoint] for endpoint in router_ttft}
-                    router_itl = {endpoint: router_itl[endpoint] / router_counts[endpoint] for endpoint in router_itl}
+                    router_scores = {
+                        endpoint: router_scores[endpoint] / router_counts[endpoint]
+                        for endpoint in router_scores
+                    }
+                    router_cost = {
+                        endpoint: router_cost[endpoint] / router_counts[endpoint]
+                        for endpoint in router_cost
+                    }
+                    router_ttft = {
+                        endpoint: router_ttft[endpoint] / router_counts[endpoint]
+                        for endpoint in router_ttft
+                    }
+                    router_itl = {
+                        endpoint: router_itl[endpoint] / router_counts[endpoint]
+                        for endpoint in router_itl
+                    }
                     total_weight = sum(router_counts.values())
-                    final_score = sum([router_scores[endpoint] * router_counts[endpoint] for endpoint in router_scores]) / total_weight
-                    final_cost = sum([router_cost[endpoint] * router_counts[endpoint] for endpoint in router_cost]) / total_weight
-                    final_ttft = sum([router_ttft[endpoint] * router_counts[endpoint] for endpoint in router_ttft]) / total_weight
-                    final_itl = sum([router_itl[endpoint] * router_counts[endpoint] for endpoint in router_itl]) / total_weight
-                    router_points.append({
-                        "model": f"router_{abs(round(a, 3))}_{abs(round(b, 3))}_{abs(round(c, 3))}",
-                        "quality": final_score, 
-                        "cost": final_cost, 
-                        "ttft": final_ttft, 
-                        "itl": final_itl
-                    })
+                    final_score = (
+                        sum(
+                            [
+                                router_scores[endpoint] * router_counts[endpoint]
+                                for endpoint in router_scores
+                            ]
+                        )
+                        / total_weight
+                    )
+                    final_cost = (
+                        sum(
+                            [
+                                router_cost[endpoint] * router_counts[endpoint]
+                                for endpoint in router_cost
+                            ]
+                        )
+                        / total_weight
+                    )
+                    final_ttft = (
+                        sum(
+                            [
+                                router_ttft[endpoint] * router_counts[endpoint]
+                                for endpoint in router_ttft
+                            ]
+                        )
+                        / total_weight
+                    )
+                    final_itl = (
+                        sum(
+                            [
+                                router_itl[endpoint] * router_counts[endpoint]
+                                for endpoint in router_itl
+                            ]
+                        )
+                        / total_weight
+                    )
+                    router_points.append(
+                        {
+                            "model": f"router_{abs(round(a, 3))}_{abs(round(b, 3))}_{abs(round(c, 3))}",
+                            "quality": final_score,
+                            "cost": final_cost,
+                            "ttft": final_ttft,
+                            "itl": final_itl,
+                        }
+                    )
         final_scores[dataset] = router_points
     return final_scores
 
 
 def get_point_solutions(data):
     final_scores = dict()
-    
+
     for dataset in data:
         prompt_list = data[dataset]
-        models = [key.replace("_score", "") for key in prompt_list[0].keys() if key != "prompt" and "pred" not in key]
+        models = [
+            key.replace("_score", "")
+            for key in prompt_list[0].keys()
+            if key != "prompt" and "pred" not in key
+        ]
         model_scores = dict()
         model_counts = dict()
-        
+
         for model in models:
             model_scores[model] = 0
             model_counts[model] = 0
@@ -191,13 +279,18 @@ def get_point_solutions(data):
                 if model + "_score" in prompt:
                     model_scores[model] += prompt[model + "_score"]
                     model_counts[model] += 1
-        
+
         dataset_scores = []
         for model in models:
-            dataset_scores.append({"model": model, "quality": round(model_scores[model] / model_counts[model], 2)})
-        
+            dataset_scores.append(
+                {
+                    "model": model,
+                    "quality": round(model_scores[model] / model_counts[model], 2),
+                }
+            )
+
         final_scores[dataset] = dataset_scores
-    
+
     return final_scores
 
 
@@ -205,7 +298,10 @@ def prune_router_points(router_points):
     final_pruned_points = dict()
     for dataset in router_points:
         print(dataset)
-        points = [[point["quality"], point["cost"], point["ttft"], point["itl"]] for point in router_points[dataset]]
+        points = [
+            [point["quality"], point["cost"], point["ttft"], point["itl"]]
+            for point in router_points[dataset]
+        ]
         if points:
             try:
                 convex_hull = scipy.spatial.ConvexHull(points)
@@ -214,34 +310,47 @@ def prune_router_points(router_points):
             except scipy.spatial._qhull.QhullError:
                 final_pruned_points[dataset] = []
                 continue
-        
-            metrics = np.array([[point["quality"], point["cost"], point["ttft"], point["itl"]] for point in convex_points])
-            normalized_metrics = (metrics - np.mean(metrics, axis=0)) / np.std(metrics, axis=0)
-        
+
+            metrics = np.array(
+                [
+                    [point["quality"], point["cost"], point["ttft"], point["itl"]]
+                    for point in convex_points
+                ]
+            )
+            normalized_metrics = (metrics - np.mean(metrics, axis=0)) / np.std(
+                metrics, axis=0
+            )
+
             num = normalized_metrics.shape[0]
             dist_matrix = np.zeros((num, num))
-            
+
             for i in range(num):
                 for j in range(num):
                     dist = np.linalg.norm(normalized_metrics[i] - normalized_metrics[j])
                     dist_matrix[i][j] = dist
                     dist_matrix[j][i] = dist
-            
+
             deleted = []
             threshold = 0.8
             for i in range(num):
                 if i not in deleted:
                     for j in range(num):
-                        if dist_matrix[i][j] < threshold and i != j and j not in deleted:
+                        if (
+                            dist_matrix[i][j] < threshold
+                            and i != j
+                            and j not in deleted
+                        ):
                             deleted.append(j)
-            
+
             print(len(deleted), num)
-            pruned_points = [point for i, point in enumerate(convex_points) if i not in deleted]
+            pruned_points = [
+                point for i, point in enumerate(convex_points) if i not in deleted
+            ]
             print(len(pruned_points))
         else:
             pruned_points = points
         final_pruned_points[dataset] = pruned_points
-    
+
     return final_pruned_points
 
 
@@ -254,11 +363,11 @@ def generate_and_prune_points(raw_responses):
     for dataset in pruned_router_points:
         final_points[dataset] = {
             "point_solutions": point_solutions[dataset],
-            "router": pruned_router_points[dataset]
+            "router": pruned_router_points[dataset],
         }
     return final_points
 
 
-raw_responses = json.load(open("temp_data.json"))
-final_points = generate_and_prune_points(raw_responses)
-json.dump(final_points, open("final_temp_results.json", "w"))
+# raw_responses = json.load(open("temp_data.json"))
+# final_points = generate_and_prune_points(raw_responses)
+# json.dump(final_points, open("final_temp_results.json", "w"))
