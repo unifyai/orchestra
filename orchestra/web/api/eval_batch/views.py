cf8aef9e10113ba14dc28d081df9d1cab3db7deb
guillesanbri <guillesanbri@users.noreply.github.com>
2024-05-15 13:25:00 +0200

    add upload and download dataset endpoints (#260)

--- orchestra/web/api/eval_batch/views.py
+++ orchestra/web/api/eval_batch/views.py
@@ -1,6 +1,5 @@
 import json
 import os
-import time
 import requests
 from typing import Annotated, Any, Dict, List
 
@@ -117,6 +116,59 @@ def training(  # noqa: C901, WPS210, WPS231, WPS211, WPS217, WPS238
     )
 
 
+@router.post("/upload_dataset")
+def upload_dataset(  # noqa: C901, WPS210, WPS231, WPS211, WPS217, WPS238
+    request_fastapi: Request,
+    file: Annotated[UploadFile, Form()],
+    name: Annotated[str, Form()],
+) -> EvalBatchResponse:
+    """
+    Upload a dataset.
+    """
+
+    # TODO: Check if format is valid
+
+    bucket_name = "uploaded_datasets"
+    blob_name = f"{request_fastapi.state.user_id}/{name}.jsonl"
+
+    exists = check_file_exists(bucket_name, blob_name)
+    if exists:
+        raise HTTPException(
+            status_code=400,
+            detail="A dataset with this name already exists. Please, choose a different one.",
+        )
+    else:
+        file_content = file.file.read()
+        upload_json_to_bucket(file_content, bucket_name, blob_name)
+
+    return EvalBatchResponse(info="Dataset uploaded succesfully!")
+
+
+@router.get("/download_dataset")
+def download_dataset(  # noqa: C901, WPS210, WPS231, WPS211, WPS217, WPS238
+    request_fastapi: Request,
+    name: str,
+) -> List[Dict[str, str]]:
+    """
+    Download a dataset.
+    """
+
+    bucket_name = "uploaded_datasets"
+    blob_name = f"{request_fastapi.state.user_id}/{name}.jsonl"
+
+    exists = check_file_exists(bucket_name, blob_name)
+    if not exists:
+        raise HTTPException(
+            status_code=400,
+            detail="This dataset does not exist.",
+        )
+    else:
+        string = read_json_from_bucket(bucket_name, blob_name, raw=True)
+        string = "[".encode() + string + "]".encode()
+        string = string.replace("}\n{".encode(), "},{".encode())
+        return json.loads(string)
+
+
 def check_file_exists(bucket_name, blob_name):
     client = storage.Client()
     bucket = client.bucket(bucket_name)
@@ -128,11 +180,13 @@ def check_file_exists(bucket_name, blob_name):
         return False
 
 
-def read_json_from_bucket(bucket_name, blob_name):
+def read_json_from_bucket(bucket_name, blob_name, raw=False):
     client = storage.Client()
     bucket = client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
     json_data = blob.download_as_string()
+    if raw:
+        return json_data
     return json.loads(json_data.decode("utf-8"))
 
 
@@ -149,8 +203,6 @@ def get_dataset_evaluation(
     dataset_name: str,
     dataset_evaluation_task_dao: DatasetEvaluationTaskDAO = Depends(),
     dataset_evaluation_dao: DatasetEvaluationDAO = Depends(),
-    endpoint_dao: EndpointDAO = Depends(),
-    benchmark_run_dao: BenchmarkRunDAO = Depends(),
 ) -> Dict[str, Dict[str, List[Dict[str, Any]]]]:
     """
     Retrieve specific dataset evaluation object from the database.
