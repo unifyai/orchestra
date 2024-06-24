87367f642f5a084a8212f7b7da3da0e903870d1b
Tom Edwards <49121269+tomatillos@users.noreply.github.com>
2024-06-24 14:23:57 +0100

    bugfixes (#309)

--- deploy_router/main.py
+++ deploy_router/main.py
@@ -42,7 +42,7 @@ def pub_sub_callback(message):
             router_name = data["router_name"]
             orchestra_url = data["orchestra_url"]
             subprocess.Popen(
-                f"venv/bin/python3 deploy_router.py --user_id={user_id} --router_name={router_name}, --orchestra_url={orchestra_url}",
+                f"venv/bin/python3 deploy_router.py --user_id={user_id} --router_name={router_name} --orchestra_url={orchestra_url}",
                 shell=True,
             )
         except json.decoder.JSONDecodeError:
