ff33d7f1a9a844bff6752ff5274dae98fee6d240
Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>
2024-06-25 19:44:06 +0530

    added support for groq (#301)
    * added support for groq
    
    * added test to the seed sql file
    
    * changed the idx of the model table
    
    * added groq as the highest number
    
    * more changes
    
    * Update tests.yml
    
    * added the groq endpoint to the endpoint table as well in the test

---
+++ providers/completion/groq.py
@@ -0,0 +1,46 @@
+from providers.completion.base_completion_provider import BaseCompletionProvider
+
+
+class Groq(BaseCompletionProvider):
+    """
+    A completion provider that uses the Groq service.
+
+    Supported models: https://console.groq.com/docs/models
+    Pricing is per million tokens: https://console.groq.com/settings/billing
+    """
+
+    def __init__(self, hub_model):
+        super().__init__(hub_model)
+        self.supported_models = supported_models
+
+    @property
+    def api_key_var(self) -> str:
+        return "ORCHESTRA_GROQ_API_KEY"
+
+    @property
+    def base_url(self):
+        return "https://api.groq.com/openai/v1"
+
+
+supported_models = {
+    "gemma-7b-it": {
+        "endpoint": "gemma-7b-it",
+        "context_window": 8192,
+        "cost": {"prompt": 0.1, "completion": 0.1},
+    },
+    "mixtral-8x7b-instruct-v0.1": {
+        "endpoint": "mixtral-8x7b-32768",
+        "context_window": 32768,
+        "cost": {"prompt": 0.27, "completion": 0.27},
+    },
+    "llama-3-8b-chat": {
+        "endpoint": "llama3-8b-8192",
+        "context_window": 8192,
+        "cost": {"prompt": 0.05, "completion": 0.1},
+    },
+    "llama-3-70b-chat": {
+        "endpoint": "llama3-70b-8192",
+        "context_window": 8192,
+        "cost": {"prompt": 0.59, "completion": 0.79},
+    },
+}
