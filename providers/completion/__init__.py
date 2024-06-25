15adf3204dfde8b8374fba0ecbf4e0baf6e1ffeb
Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>
2024-06-25 19:44:06 +0530

    added support for groq (#301)
    * added support for groq
    
    * added test to the seed sql file
    
    * changed the idx of the model table
    
    * added groq as the highest number
    
    * more changes
    
    * Update tests.yml
    
    * added the groq endpoint to the endpoint table as well in the test

--- providers/completion/__init__.py
+++ providers/completion/__init__.py
@@ -3,8 +3,10 @@
 from providers.completion.anthropic import Anthropic
 from providers.completion.anyscale import Anyscale
 from providers.completion.bedrock import AWSBedrock
+from providers.completion.custom_provider import CustomProvider
 from providers.completion.deepinfra import Deepinfra
 from providers.completion.fireworksai import FireworksAI
+from providers.completion.groq import Groq
 from providers.completion.leptonai import LeptonAI
 from providers.completion.mistral import Mistral
 from providers.completion.octoai import OctoAI
@@ -12,7 +14,6 @@ from providers.completion.openai import OpenAI
 from providers.completion.perplexity import Perplexity
 from providers.completion.replicate import Replicate
 from providers.completion.togetherai import TogetherAI
-from providers.completion.custom_provider import CustomProvider
 
 PROVIDER_CLASSES = {
     "anyscale": Anyscale,
@@ -23,6 +24,7 @@ PROVIDER_CLASSES = {
     "openai": OpenAI,
     "mistral-ai": Mistral,
     "octoai": OctoAI,
+    "groq": Groq,
     "lepton-ai": LeptonAI,
     "fireworks-ai": FireworksAI,
     "deepinfra": Deepinfra,
