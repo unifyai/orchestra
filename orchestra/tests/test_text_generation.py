c81923b1f53071b7e0f7979f59b62bab69198bc4
guillesanbri <Guillesanbri@hotmail.com>
2024-06-26 14:26:22 +0200

    VertexAI (#317)
    * added vertex ai with gemini-1.5 pro/flash, added llama-3 models to bedrock (#306)
    
    * added vertex ai with gemini-1.5 pro/flash, added llama-3 models to bedrock
    
    * updated max tokens
    
    * removed llama-2-13b-chat from the aws test
    
    * added the gemini test
    
    * tried adding vertexai credentials to the environment
    
    * minor changeg
    
    * minor change
    
    * added gemini related fail checks for response test
    
    * updated service account
    
    * minor change
    
    * added login steps for gcloud sdk
    
    * updated vertexai credentials
    
    * added vertex api key env
    
    * added vertex api key env
    
    * minor change
    
    * removed env var modification with os.environ
    
    * fixed a stupid typo
    
    * tried removing setup gcloud from the workflow
    
    * minor change
    
    * default to now instead of none if creation is not available
    
    * change schema
    
    * add id value
    
    * clean up service account loading a bit
    
    * fix test
    
    * change url to ue settings
    
    ---------
    
    Co-authored-by: guillesanbri <Guillesanbri@hotmail.com>
    
    * clean up comments
    
    * remove cloudbuild optimisations
    
    * add fields to inference if missing
    
    * same with streaming
    
    ---------
    Co-authored-by: Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>

--- orchestra/tests/test_text_generation.py
+++ orchestra/tests/test_text_generation.py
@@ -24,10 +24,10 @@ MODELS = [
     "llama-2-7b-chat@lepton-ai",
     "llama-2-7b-chat@replicate",
     "llama-2-7b-chat@together-ai",
-    "llama-2-13b-chat@aws-bedrock",
     "mistral-7b-instruct-v0.2@aws-bedrock",
     "mistral-7b-instruct-v0.2@mistral-ai",
     "mistral-7b-instruct-v0.2@octoai",
+    "gemini-1.5-flash@vertex-ai",
     "llama-3-8b-chat@perplexity-ai",
     "llama-3-8b-chat@groq",
 ]
