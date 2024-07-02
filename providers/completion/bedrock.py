963df21399cfe1048a4a517f08e6d75fba215ee4
Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>
2024-07-02 09:14:12 +0000

    minor change

--- providers/completion/bedrock.py
+++ providers/completion/bedrock.py
@@ -304,7 +304,7 @@ supported_models = {
         "context_window": 32000,
         "cost": {"prompt": 4, "completion": 12},
     },
-    "command-r+": {
+    "command-r-plus": {
         "endpoint": "cohere.command-r-plus-v1:0",
         "context_window": 128000,
         "cost": {"prompt": 3, "completion": 15},
