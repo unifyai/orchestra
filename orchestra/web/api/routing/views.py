076ddd011fcaf17f22d7c9e5946433e24257964a
guillesanbri <Guillesanbri@hotmail.com>
2024-07-03 16:12:56 +0200

    Deploy router endpoints (#337)
    * add initial signatures
    
    * share gcp utils
    
    * add incomplete endpoints
    
    * configure publishers and create topics
    
    * wip util functions
    
    * change namespace to avoid conflict with router file
    
    * add vertex ai endpoint check fn

---
+++ orchestra/web/api/routing/views.py
@@ -0,0 +1,184 @@
+"""
+Includes endpoints for training and deployment of a router.
+"""
+
+from typing import Dict, List
+
+from fastapi import APIRouter, Request
+from providers.completion import PROVIDER_CLASSES
+
+from orchestra.web.api.utils.gcp import (
+    blob_exists,
+    send_pubsub_msg,
+    vertex_ai_endpoint_exists,
+)
+from orchestra.web.api.utils.http_responses import (
+    dataset_does_not_exist,
+    invalid_training_endpoints,
+    router_already_deployed,
+    router_is_not_deployed,
+    router_training_already_exists,
+    router_training_does_not_exist,
+)
+
+router = APIRouter()
+
+# utils
+
+
+def dataset_exists(user_id, name):
+    # TODO: This needs to take public datasets into account as
+    # well.
+    bucket_name = "uploaded_datasets"
+    blob_name = f"{user_id}/{name}/0/dataset.jsonl"
+    if blob_exists(bucket_name, blob_name):
+        return True
+    return False
+
+
+def router_training_exists(user_id, name):
+    # TODO: The router directory with files needs a
+    # metadata.json with the datasets it has been trained on
+    bucket_name = "custom_router_data"
+    dir = f"custom_router/{user_id}/{name}/"
+    files = ["config.yaml", "model_mapping.json", "model.pth"]
+    for f in files:
+        if not blob_exists(bucket_name, dir + f):
+            return False
+    return True
+
+
+def router_is_deployed(user_id, name):
+    # TODO: Implement checks to ensure no one can deploy a router if
+    # the name is already in use.
+    endpoint = f"{user_id}_{name}"
+    if vertex_ai_endpoint_exists(endpoint):
+        return True
+    return False
+
+
+def is_standard_endpoint(model: str, provider: str):
+    if provider in PROVIDER_CLASSES:
+        lm = PROVIDER_CLASSES["provider"](model)
+        if model in lm.supported_models:
+            return True
+    return False
+
+
+def find_invalid_endpoints(endpoints):
+    invalid_endpoints = []
+    for e in endpoints:
+        model, provider = e.split("@")
+        if "router" in model:
+            invalid_endpoints.append(e)
+            continue
+        if provider == "custom":
+            # TODO: Support this properly, probably all providers (including custom one)
+            # can have a method to check if the model exists as an endpoint
+            # We also need an endpoint to list all public + custom endpoints
+            invalid_endpoints.append(e)  # temp
+            continue
+        if not is_standard_endpoint(model, provider):
+            invalid_endpoints.append(e)  # temp
+            continue
+    return invalid_endpoints
+
+
+def send_to_train_server(action, **data):
+    topic = "projects/saas-368716/topics/train_router"
+    url = "https://api.unify.ai"  # TODO: Deal with staging/test
+    send_pubsub_msg(topic, {"action": action, **data, "orchestra_url": url})
+
+
+def send_to_deploy_server(action, **data):
+    topic = "projects/saas-368716/topics/deploy_router"
+    url = "https://api.unify.ai"  # TODO: Deal with staging/test
+    send_pubsub_msg(topic, {"action": action, **data, "orchestra_url": url})
+
+
+# endpoints
+
+# TODO: Allow not sending an email for the website + composed flows
+# TODO: List trained routers
+
+
+@router.post("router/train")
+def train_router(
+    request_fastapi: Request,
+    name: str,
+    dataset: str,
+    endpoints: List[str],
+) -> Dict[str, str]:
+    user_id = request_fastapi.state.user_id
+    # Check if the router already exists
+    if router_training_exists(user_id, name):
+        raise router_training_already_exists
+    # Check if the dataset exists
+    if not dataset_exists(user_id, dataset):
+        raise dataset_does_not_exist
+    # Check that the endpoints are valid
+    invalid_endpoints = find_invalid_endpoints(endpoints)
+    if invalid_endpoints:
+        raise invalid_training_endpoints(invalid_endpoints)
+    # Send train job to the training server
+    send_to_train_server(
+        action="train",
+        user_id=user_id,
+        name=name,
+        dataset=dataset,
+        endpoints=endpoints,
+    )
+    return {"info": "Router training started! You will receive an email soon!"}
+
+
+@router.delete("/router/train")
+def delete_router_train(request_fastapi: Request, name: str) -> Dict[str, str]:
+    """
+    Deactivates and deletes a trained router.
+    """
+    user_id = request_fastapi.state.user_id
+    # Check if the router files exist
+    if not router_training_exists(user_id, name):
+        raise router_training_does_not_exist
+    # Delete the trained router files
+    send_to_train_server(action="delete", user_id=user_id, name=name)
+    #   delete_training_files(router)
+    #   set_router_training_status("deleted")
+    #   router training -> id, user_id, name, dataset, status
+    return {"info": "Trained router deleted!"}
+
+
+# TODO: List deployed routers
+
+
+@router.post("/router/deploy")
+def deploy_router(request_fastapi: Request, name: str) -> Dict[str, str]:
+    """
+    Deploys a router.
+    """
+    user_id = request_fastapi.state.user_id
+    # Check if the files exist
+    if not router_training_exists(user_id, name):
+        raise router_training_does_not_exist
+    # Check if the router is already deployed
+    if router_is_deployed(user_id, name):
+        raise router_already_deployed
+    # Send the request with the job to the router deployment service
+    send_to_deploy_server(action="deploy", user_id=user_id, name=name)
+    return {"info": "Router deployment started! You will receive an email soon!"}
+
+
+@router.delete("/router/deploy")
+def delete_router(request_fastapi: Request, name: str) -> Dict[str, str]:
+    """
+    Deactivates and deletes a deployed router.
+    """
+    user_id = request_fastapi.state.user_id
+    # Check if the router exists
+    if not router_is_deployed(user_id, name):
+        raise router_is_not_deployed
+    # Send the request with the job to the router deployment service
+    send_to_deploy_server(action="delete", user_id=user_id, name=name)
+    #   un-deploy router
+    #   modify entry in the db
+    return {"info": "Router deletion started! You will receive an email soon!"}
---
+++ orchestra/web/api/utils/gcp.py
@@ -0,0 +1,97 @@
+import json
+from typing import Dict
+
+from google.cloud import aiplatform, pubsub_v1, storage
+from google.cloud.exceptions import NotFound
+
+# Pub/Sub
+
+
+def send_pubsub_msg(topic: str, msg: Dict[str, str]) -> None:
+    # TODO: Make sure this sends msgs correctly in:
+    # - Staging
+    # - Local
+    # - Tests / CI
+
+    # To instantiate with specific credentials
+    # from google.oauth2 import service_account
+    # key_path = "./archive/pubsub_2_clickhouse.json"
+    # credentials = service_account.Credentials.from_service_account_file(key_path)
+    # publisher = pubsub_v1.PublisherClient(credentials=credentials)
+
+    publisher = pubsub_v1.PublisherClient()
+    future = publisher.publish(topic, json.dumps(msg).encode())
+    future.result()
+
+
+# Cloud Storage
+
+
+def blob_exists(bucket_name: str, blob_name: str) -> bool:
+    blob = storage.Client().bucket(bucket_name).blob(blob_name)
+    try:
+        blob.reload()
+    except NotFound:
+        return False
+    return True
+
+
+def dir_exists(bucket_name: str, dir_name: str) -> bool:
+    bucket = storage.Client().bucket(bucket_name)
+    blobs = list(bucket.list_blobs(prefix=dir_name))
+    return len(blobs) > 0
+
+
+def delete_dir(bucket_name: str, dir_name: str) -> None:
+    bucket = storage.Client().bucket(bucket_name)
+
+    # Ensure the directory_name ends with a slash
+    if not dir_name.endswith("/"):
+        dir_name += "/"
+
+    # List all blobs with the directory_name prefix
+    blobs = bucket.list_blobs(prefix=dir_name)
+
+    # Delete each blob
+    for blob in blobs:
+        blob.delete()
+
+
+def list_dir(bucket_name: str, prefix: str):
+    bucket = storage.Client().bucket(bucket_name)
+    # List blobs with the specified prefix
+    return list(bucket.list_blobs(prefix=prefix))
+
+
+def read_json_from_bucket(bucket_name, blob_name, raw=False):
+    blob = storage.Client().bucket(bucket_name).blob(blob_name)
+    json_data = blob.download_as_bytes()
+    if raw:
+        return json_data
+    return json.loads(json_data.decode("utf-8"))
+
+
+def upload_json_to_bucket(json_data: Dict[str, str], bucket_name: str, blob_name: str):
+    blob = storage.Client().bucket(bucket_name).blob(blob_name)
+    blob.upload_from_string(json_data, content_type="application/json")
+
+
+# VertexAI
+
+
+def vertex_ai_endpoint_exists(name: str) -> bool:
+    region = "europe-west1"
+    project_id = "saas-368716"
+    client_options = {"api_endpoint": f"{region}-aiplatform.googleapis.com"}
+    client = aiplatform.gapic.EndpointServiceClient(client_options=client_options)
+
+    # Specify the parent resource
+    parent = f"projects/{project_id}/locations/{region}"
+
+    # List the endpoints
+    endpoints = client.list_endpoints(parent=parent)
+
+    for endpoint in endpoints:
+        if endpoint.display_name == name:
+            return True
+    return False
