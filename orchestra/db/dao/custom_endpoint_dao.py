a30ae814852a15f8c321ac5bdf5db683fe6a9eff
Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>
2024-07-25 14:25:02 +0000

    got the custom endpoints and api key endpoints working with local files

--- orchestra/db/dao/custom_endpoint_dao.py
+++ orchestra/db/dao/custom_endpoint_dao.py
@@ -1,10 +1,13 @@
-from typing import Any, List, Tuple
+import os
+from typing import List, Tuple
+
 from fastapi import Depends
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from orchestra.db.dependencies import get_db_session
 from orchestra.db.models.orchestra_models import CustomApiKey, CustomEndpoint
+from orchestra.web.api.utils.on_prem import OnPremModel
 
 
 class CustomEndpointDAO:
@@ -12,33 +15,58 @@ class CustomEndpointDAO:
 
     def __init__(self, session: Session = Depends(get_db_session)):
         self.session = session
+        self.on_prem = os.environ.get("ON_PREM")
+        if self.on_prem:
+            self.on_prem_model = OnPremModel(
+                model_class=CustomEndpoint,
+                table_name="custom_endpoint",
+            )
 
     def create_custom_endpoint(
-        self, user_id: str, name: str, url: str, key_id: int
+        self,
+        user_id: str,
+        name: str,
+        url: str,
+        key_id: int,
     ) -> None:
-        self.session.add(
-            CustomEndpoint(user_id=user_id, name=name, url=url, key_id=key_id)
-        )
+        data = {"user_id": user_id, "name": name, "url": url, "key_id": key_id}
+        if self.on_prem:
+            self.on_prem_model.create(**data)
+        else:
+            self.session.add(
+                CustomEndpoint(user_id=user_id, name=name, url=url, key_id=key_id),
+            )
 
     def filter(self, user_id: str, name: str) -> List[CustomEndpoint]:
+        if self.on_prem:
+            return self.on_prem_model.read(
+                filters={
+                    "custom_endpoint": {"user_id": user_id, "name": name},
+                },
+            )
         query = (
             select(CustomEndpoint)
             .where(CustomEndpoint.user_id == user_id)
             .where(CustomEndpoint.name == name)
         )
-
         raw_custom_endpoints = self.session.execute(query)
-
         return list(raw_custom_endpoints.scalars().fetchall())
 
     def get_user_endpoints(self, user_id: str) -> List[Tuple[str, str, str]]:
-
+        if self.on_prem:
+            return self.on_prem_model.read(
+                filters={"custom_endpoint": {"user_id": user_id}},
+                join_table="custom_api_key",
+                join_columns=["key_id", "id"],
+                select_columns={
+                    "custom_endpoint": ["name", "url"],
+                    "custom_api_key": ["key"],
+                },
+            )
         query = (
             select(CustomEndpoint.name, CustomEndpoint.url, CustomApiKey.key)
             .join(CustomApiKey, CustomEndpoint.key_id == CustomApiKey.id)
             .where(CustomEndpoint.user_id == user_id)
         )
-
         raw_custom_endpoints = self.session.execute(query)
-
         return list(raw_custom_endpoints.fetchall())
--- orchestra/web/api/utils/on_prem.py
+++ orchestra/web/api/utils/on_prem.py
@@ -1,11 +1,96 @@
 import json
 import os
 import shutil
-from typing import Dict
+from functools import reduce
+from typing import Dict, List, Tuple, Union
 
 import redis
 
 shared_volume = os.environ.get("SHARED_VOLUME")
+
+
+class OnPremModel:
+    def __init__(self, model_class: object, table_name: str):
+        self.model_class = model_class
+        self.table_name = table_name
+        self.shared_volume = shared_volume
+        self.json_path = os.path.join(self.shared_volume, "db", f"{table_name}.json")
+        if not os.path.exists(self.json_path):
+            os.makedirs(os.path.join(self.shared_volume, "db"), exist_ok=True)
+            with open(self.json_path, "w") as f:
+                json.dump({"data": []}, f)
+
+    def create(self, **data):
+        with open(self.json_path) as f:
+            entries = json.load(f)["data"]
+        id = (entries[-1]["id"] + 1) if len(entries) else 0
+        data["id"] = id
+        with open(self.json_path, "w") as f:
+            json.dump({"data": [*entries, data]}, f)
+
+    def read(
+        self,
+        filters: Dict[str, Dict[str, Union[str, int]]],
+        join_table: str = None,
+        join_columns: Tuple[str, str] = None,
+        select_columns: Dict[str, List[str]] = None,
+    ):
+        # getting the entries for the primary table
+        with open(self.json_path) as f:
+            entries = json.load(f)["data"]
+
+        # getting the entries for the joined table if required
+        # the entries for the joined table would be corresponding
+        # to those in the primary table
+        final_entries = {self.table_name: entries}
+        if join_table and join_columns:
+            with open(
+                os.path.join(self.shared_volume, "db", f"{join_table}.json"),
+            ) as f:
+                join_entries = json.load(f)["data"]
+                join_entries = reduce(
+                    lambda x, y: {**x, **y},
+                    [{entry[join_columns[1]]: entry} for entry in join_entries],
+                )
+            final_entries[join_table] = [
+                join_entries[entry[join_columns[0]]]
+                for entry in final_entries[self.table_name]
+            ]
+
+        # filter the entries based on the filter columns
+        filtered = False
+        for i in range(len(final_entries[self.table_name]) - 1, -1, -1):
+            filtered = False
+            for table_name in filters:
+                fields = list(filters[table_name].keys())
+                for field in fields:
+                    if (
+                        filters[table_name][field] is not None
+                        and filters[table_name][field]
+                        != final_entries[table_name][i][field]
+                    ):
+                        [entry.pop(i) for entry in final_entries.values()]
+                        filtered = True
+                        break
+                if filtered:
+                    break
+
+        # we do have select columns almost always when doing joins, so don't need to deal with joins here
+        if not select_columns:
+            return [
+                self.model_class(**entry) for entry in final_entries[self.table_name]
+            ]
+
+        # select needed columns
+        final_results = []
+        for i in range(len(final_entries[self.table_name])):
+            result = {}
+            for table_name in select_columns:
+                for column in select_columns[table_name]:
+                    result[column] = final_entries[table_name][i][column]
+            final_results.append(result)
+
+        return final_results
 
 
 def send_pubsub_msg(topic: str, msg: Dict[str, str]) -> None:
