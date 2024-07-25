6ad8faab5eb608e5c1a55fb1ee2c5b75f4ba7e15
tje <tomatillos@users.noreply.github.com>
2024-07-25 11:31:33 +0100

    update version to fix test

--- orchestra/tests/test_text_generation.py
+++ orchestra/tests/test_text_generation.py
@@ -89,7 +89,7 @@ async def test_text_generation(  # noqa: WPS218, E501
 async def test_fallback_parse(
     client: AsyncClient,
 ) -> None:
-    model_str = "claude-3-haiku@anthropic->mistral-7b-instruct-v0.2@octoai"
+    model_str = "claude-3-haiku@anthropic->mistral-7b-instruct-v0.3@octoai"
     endpoint = "/v0/chat/completions"
     data = get_chat_completions_payload_fallback(model_str, stream=False)
     response = await client.post(endpoint, headers=HEADERS, json=data)
@@ -104,13 +104,13 @@ async def test_fallback_parse(
 async def test_fallback_after_fail(
     client: AsyncClient,
 ) -> None:
-    model_str = "FAKE_MODEL@anthropic->mistral-7b-instruct-v0.2@octoai"
+    model_str = "FAKE_MODEL@anthropic->mistral-7b-instruct-v0.3@octoai"
     endpoint = "/v0/chat/completions"
     data = get_chat_completions_payload_fallback(model_str, stream=False)
     response = await client.post(endpoint, headers=HEADERS, json=data)
     assert response.status_code == status.HTTP_200_OK
     response_json = json.loads(response.text)
-    assert response_json["model"] == "mistral-7b-instruct-v0.2@octoai"
+    assert response_json["model"] == "mistral-7b-instruct-v0.3@octoai"
     check_text_gen_response(response_json, "chat.completion")
     check_text_gen_choice(response_json.get("choices")[0], "message")
     check_text_gen_usage(response_json.get("usage"))
