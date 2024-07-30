393a88056733f06cc1bf19d0f0f1baab1a9a9eab
guillesanbri <Guillesanbri@hotmail.com>
2024-07-30 13:26:19 +0000

    remove license endpoints

--- orchestra/web/api/admin/views.py
+++ orchestra/web/api/admin/views.py
@@ -14,7 +14,6 @@ from orchestra.db.dao.datapoint_dao import DatapointDAO
 from orchestra.db.dao.dataset_evaluation_dao import DatasetEvaluationDAO
 from orchestra.db.dao.dataset_evaluation_task_dao import DatasetEvaluationTaskDAO
 from orchestra.db.dao.endpoint_dao import EndpointDAO
-from orchestra.db.dao.license_dao import LicenseDAO
 from orchestra.db.dao.metric_dao import MetricDAO
 from orchestra.db.dao.modality_dao import ModalityDAO
 from orchestra.db.dao.model_dao import ModelDAO
@@ -315,38 +314,6 @@ def get_endpoint(
     )
 
 
-@router.get("/get_all_licenses", response_model=List[LicenseModelResponse])
-def get_license_models(
-    limit: int = 10,
-    offset: int = 0,
-    license_dao: LicenseDAO = Depends(),
-) -> List[License]:
-    """
-    Retrieve all license objects from the database.
-
-    :param limit: limit of license objects, defaults to 10.
-    :param offset: offset of license objects, defaults to 0.
-    :param license_dao: DAO for license models.
-    :return: list of license objects from database.
-    """
-    return license_dao.get_all_licenses(limit=limit, offset=offset)
-
-
-@router.get("/get_license", response_model=List[LicenseModelResponse])
-def get_license(
-    name: str,
-    license_dao: LicenseDAO = Depends(),
-) -> List[License]:
-    """
-    Retrieve specific license object from the database.
-
-    :param name: name of license instance.
-    :param license_dao: DAO for license models.
-    :return: list of license objects from database.
-    """
-    return license_dao.filter(name=name)
-
-
 @router.get("/get_all_metrics", response_model=List[MetricModelResponse])
 def get_metric_models(
     limit: int = 10,
@@ -557,24 +524,6 @@ def create_endpoint_model(
         mdl_id=new_endpoint_object.mdl_id,
         provider_id=new_endpoint_object.provider_id,
         created_at=created_at,
-    )
-
-
-@router.put("/create_license")
-def create_license_model(
-    new_license_object: LicenseModelRequest,
-    license_dao: LicenseDAO = Depends(),
-) -> None:
-    """
-    Creates license model in the database.
-
-    :param new_license_object: new license model item.
-    :param license_dao: DAO for license models.
-    """
-    license_dao.create_license(
-        name=new_license_object.name,
-        image_url=new_license_object.image_url,
-        description=new_license_object.description,
     )
 
 
