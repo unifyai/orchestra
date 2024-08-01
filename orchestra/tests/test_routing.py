513a96c7fe0af9751d17fea8b990f1fc58387ca8
tje <tje541@gmail.com>
2024-08-01 18:00:18 +0100

    changes to test

--- orchestra/tests/test_routing.py
+++ orchestra/tests/test_routing.py
@@ -27,18 +27,29 @@ async def test_train_router(client: AsyncClient):
     router_name = f"test_router_train_{int(time.time()*1000 % 100000)}"
     dataset_name = "test_train_router"
     endpoints = ["llama-3.1-8b-chat@aws-bedrock", "claude-3-haiku@aws-bedrock"]
-    dataset = _upload_dataset(client, dataset_name=dataset_name, data_path=sample_path)
-    params = {"name": router_name, "dataset": dataset_name, "endpoints": endpoints}
-    response = await client.post("/v0/dataset", data=data, files=files, headers=HEADERS)
+    response = await _upload_dataset(client, dataset_name=dataset_name, data_path=sample_path)
     assert response.status_code == 200
 
-    # how do you check if it actually trains?
+    params = {"name": router_name, "dataset": dataset_name, "endpoints": endpoints}
+    response = await client.post(url, params=params, headers=HEADERS)
+    assert response.status_code == 200
+
+    # check if it's actually trained
     for tries in range(30*60):
         url = "/v0/router/train/list"
         response = await client.get(url, headers=HEADERS)
-        if router_name not in response:
+        if router_name not in response.json():
             asyncio.sleep(60)
 
+    # delete it ?
+    url = "/v0/router/train"
+    params = {"name": router_name}
+    response = await client.delete(url, params=params, headers=HEADERS)
+    assert response.status_code == 200
+    
+    url = "/v0/router/train/list"
+    response = await client.get(url, headers=HEADERS)
+    assert router_name not in response.json()
 
     assert False
 
