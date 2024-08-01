01fd32d1bfa4aafc4246cf3092f6f4488c39ae6e
tje <tje541@gmail.com>
2024-08-01 11:02:58 +0100

    add tests

---
+++ orchestra/tests/test_routing.py
@@ -0,0 +1,69 @@
+import asyncio
+from httpx import AsyncClient
+
+from orchestra.tests.utils import HEADERS
+
+
+
+## UTILS
+
+
+sample_path = "./orchestra/tests/sample_datasets/with_ref.jsonl"
+
+def _upload_dataset(client, dataset_name, data_path):
+    data = {"name": dataset_name}
+    with open(data_path, "rb") as f:
+        file_content = f.read()
+    files = {"file": ("test.jsonl", file_content, "application/x-jsonlines")}
+    response = client.post("/v0/dataset", data=data, files=files, headers=HEADERS)
+    return response
+
+
+## TESTS
+
+
+async def test_train_router(client: AsyncClient):
+    url = "/v0/router/train"
+    router_name = f"test_router_train_{int(time.time()*1000 % 100000)}"
+    dataset_name = "test_train_router"
+    endpoints = ["llama-3.1-8b-chat@aws-bedrock", "claude-3-haiku@aws-bedrock"]
+    dataset = _upload_dataset(client, dataset_name=dataset_name, data_path=sample_path)
+    params = {"name": router_name, "dataset": dataset_name, "endpoints": endpoints}
+    response = await client.post("/v0/dataset", data=data, files=files, headers=HEADERS)
+    assert response.status_code == 200
+
+    # how do you check if it actually trains?
+    for tries in range(30*60):
+        url = "/v0/router/train/list"
+        response = await client.get(url, headers=HEADERS)
+        if router_name not in response:
+            asyncio.sleep(60)
+
+
+    assert False
+
+
+def test_train_delete_router(client: AsyncClient):
+    url = "/v0/router/train"
+    assert False
+
+
+def test_train_list_router(client: AsyncClient):
+    url = "/v0/router/train/list"
+    assert False
+
+###
+
+def test_deploy_router(client: AsyncClient):
+    url = "/v0/router/deploy"
+    assert False
+
+
+def test_deploy_delete_router(client: AsyncClient):
+    url = "/v0/router/deploy"
+    assert False
+
+
+def test_deploy_list_router(client: AsyncClient):
+    url = "/v0/router/deploy/list"
+    assert False
