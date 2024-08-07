021f1cf17668d15d8128d2208a41d1b38d5b2f9f
tje <tje541@gmail.com>
2024-08-07 10:36:29 +0100

    moved custom benchmark to standard benchmark api

--- orchestra/web/api/benchmarks/views.py
+++ orchestra/web/api/benchmarks/views.py
@@ -2,12 +2,15 @@
 Includes endpoints related to benchmarks.
 """
 
-from fastapi import APIRouter
+import datetime
+from fastapi import APIRouter, Request, Query
 from fastapi.param_functions import Depends
 
 from orchestra.db.dao.benchmark_run_dao import BenchmarkRunDAO
 from orchestra.db.dao.endpoint_dao import EndpointDAO
 from orchestra.db.dao.latest_benchmark_dao import LatestBenchmarkDAO
+from orchestra.db.dao.custom_endpoint_dao import CustomEndpointDAO
+from orchestra.db.dao.custom_endpoint_benchmark_dao import CustomEndpointBenchmarkDAO
 from orchestra.web.api.utils.http_responses import benchmark_not_found, model_not_found
 from orchestra.web.api.utils.on_prem import handle_on_prem
 
@@ -31,16 +34,58 @@ def _get_endpoint_from_model_provider(
 
 
 @router.get("/benchmarks")
-@handle_on_prem(endpoint="/benchmarks", method="get")
 def get_latest_benchmark(
-    model: str,
-    provider: str,
-    regime: str = "concurrent-1",
-    region: str = "Belgium",
-    seq_len: str = "short",
+    request_fastapi: Request,
+    model: str = Query(..., description="Model name"),
+    provider: str = Query(..., description="Provider name"),
+    regime: str = Query(default="concurrent-1"),
+    region: str = Query(default="Belgium", description="""Region where the benchmark is run. Options are: "Belgium", "Hong Kong", "Iowa"."""),
+    seq_len: str = Query(default="short"),
     endpoint_dao: EndpointDAO = Depends(),
     latest_benchmark_dao: LatestBenchmarkDAO = Depends(),
+    custom_endpoint_dao: CustomEndpointDAO = Depends(),
+    custom_endpoint_benchmark_dao: CustomEndpointBenchmarkDAO = Depends(),
 ):
+    if provider == "custom":
+        try:
+            user_id = request_fastapi.state.user_id
+            available_endpoints = custom_endpoint_dao.filter(
+                user_id=user_id, name=model
+            )
+            for endpoint in available_endpoints:
+                if model == endpoint.name:
+                    endpoint_id = endpoint.id
+                    break
+            else:
+                raise HTTPException(
+                    status_code=400,
+                    detail=f"""The endpoint: {model} was not found in your account.""",
+                )
+
+            short_name_to_db_name = {
+                "ttft": "time-to-first-token",
+                "itl": "inter-token-latency",
+                "input-cost": "input-cost",
+                "output-cost": "output-cost",
+            }
+            ret = {}
+            for short_name, db_name in short_name_to_db_name.items():
+                results = custom_endpoint_benchmark_dao.benchmarks_between(
+                    endpoint_id=endpoint_id,
+                    metric_name=db_name,
+                    start_time="2024-01-01",
+                    end_time=str(datetime.datetime.now()),
+                )
+                if results:
+                    results.sort(key=lambda x: x.measured_at)
+                    result = results[-1].value
+                else:
+                    result = None
+                ret[short_name] = result
+            ret["measured_at"] = None
+            return ret
+        except:
+            raise Exception
     try:
         endpoint_id = _get_endpoint_from_model_provider(model, provider, endpoint_dao)
         result = latest_benchmark_dao.get_latest_benchmarks(
