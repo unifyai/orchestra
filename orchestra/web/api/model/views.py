3ea6378e8229178e53e52932775a5479aba6f6c4
guillesanbri <Guillesanbri@hotmail.com>
2024-08-09 11:51:06 +0000

    add private endpoints and change endpoint response format

--- orchestra/web/api/model/views.py
+++ orchestra/web/api/model/views.py
@@ -1,20 +1,17 @@
-import datetime
-from typing import List, Optional
+from typing import List
 
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, Request
 from fastapi.param_functions import Depends
 
+from orchestra.db.dao.custom_endpoint_dao import CustomEndpointDAO
 from orchestra.db.dao.endpoint_dao import EndpointDAO
-from orchestra.db.dao.model_dao import ModelDAO
 from orchestra.db.models.orchestra_models import Model
-from orchestra.web.api.model.schema import ModelResponse
 from orchestra.web.api.utils.on_prem import handle_on_prem
 
 router = APIRouter()
-public_router = APIRouter()
 
 
-@public_router.get(
+@router.get(
     "/models",
     response_model=List[str],
     responses={
@@ -30,46 +27,26 @@ public_router = APIRouter()
 )
 @handle_on_prem(endpoint="/models", method="get")
 def get_models(
+    fastapi_request: Request,
     provider: str = Query(
         default=None,
         description="Provider to get available models from.",
     ),
-    model_dao: ModelDAO = Depends(),
     endpoint_dao: EndpointDAO = Depends(),
+    custom_endpoint_dao: CustomEndpointDAO = Depends(),
 ) -> List[Model]:
     """
     Lists available models. If a provider is specified, returns the models that the provider supports.
     """
+    user_id = fastapi_request.state.user_id
+
     raw = endpoint_dao.get_endpoints_of((None,), (provider,))
-    ret = list(set([r.Model.mdl_code for r in raw]))
-    ret.sort()
-    return ret
+    models = list(set([r.Model.mdl_code for r in raw]))
 
+    private_endpoints_raw = custom_endpoint_dao.get_user_endpoints(user_id=user_id)
+    private_models = [e[0] for e in private_endpoints_raw]
 
-@router.get("/get_model", response_model=List[ModelResponse])
-def get_model(  # noqa: WPS211, C901
-    id: Optional[int] = None,  # noqa: WPS125
-    mdl_code: Optional[str] = None,
-    uploaded_at: Optional[datetime.datetime] = None,
-    task: Optional[str] = None,
-    active: Optional[bool] = None,
-    model_dao: ModelDAO = Depends(),
-) -> List[Model]:
-    """
-    Retrieve specific model object from the database.
-    \f
-    :param id: id of model instance.
-    :param mdl_code: mdl_code of model instance.
-    :param uploaded_at: uploaded_at of model instance.
-    :param task: task of model instance.
-    :param active: is model instance active.
-    :param model_dao: DAO for model models.
-    :return: list of model objects from database.
-    """
-    return model_dao.filter(
-        id=id,
-        mdl_code=mdl_code,
-        uploaded_at=uploaded_at,
-        task=task,
-        active=active,
-    )
+    if provider and provider != "custom":
+        private_models = []
+
+    return sorted(models + private_models)
