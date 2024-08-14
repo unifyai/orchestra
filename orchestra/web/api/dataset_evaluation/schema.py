a4e76704b075c9ad17a890f8aa46ed1fed5274b0
Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>
2024-08-14 15:39:30 +0000

    added json_schema_extra

--- orchestra/web/api/dataset_evaluation/schema.py
+++ orchestra/web/api/dataset_evaluation/schema.py
@@ -4,11 +4,11 @@ from pydantic import BaseModel, Field
 
 
 class EvalConfig(BaseModel):
-    eval_name: str = Field(..., example="eval1")
+    eval_name: str = Field(..., json_schema_extra={"example": "eval1"})
     system_prompt: Union[str, None] = None
     class_config: Union[dict, None] = None
     judge_models: Union[str, list[str], None] = Field(
         "claude-3.5-sonnet@aws-bedrock",
-        example="claude-3.5-sonnet@aws-bedrock",
+        json_schema_extra={"example": "claude-3.5-sonnet@aws-bedrock"},
     )
     client_side: bool = False
