1996f208d965010323e00d2aa1e13b67107c9895
guillesanbri <guillesanbri@users.noreply.github.com>
2024-08-20 12:22:26 +0000

    add admin endpoint to trigger evals in behalf of a user

--- orchestra/web/api/dataset_evaluation/__init__.py
+++ orchestra/web/api/dataset_evaluation/__init__.py
@@ -1,5 +1,5 @@
 """Dataset evaluation API."""
 
-from orchestra.web.api.dataset_evaluation.views import router
+from orchestra.web.api.dataset_evaluation.views import admin_router, router
 
-__all__ = ["router"]
+__all__ = ["router", "admin_router"]
