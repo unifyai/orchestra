369ce809822d39b7ff6658658b4c0ef0861f7c9b
tje <tje541@gmail.com>
2024-09-03 12:32:56 +0100

    migrations

---
+++ orchestra/db/migrations/versions/2024-09-03-12-27_4251682afa02.py
@@ -0,0 +1,23 @@
+"""archive query -> query_old
+
+Revision ID: 4251682afa02
+Revises: 69fda243ec6f
+Create Date: 2024-09-03 12:27:15.827936
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "4251682afa02"
+down_revision = "69fda243ec6f"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.rename_table('query', 'query_old')
+
+
+def downgrade() -> None:
+    op.rename_table('query_old', 'query')
