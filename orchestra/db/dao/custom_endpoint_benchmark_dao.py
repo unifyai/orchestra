5cf4b04823ceb66b749a6731492e24841fa89537
Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>
2024-09-05 15:14:47 +0000

    got custom endpoint benchmark endpoints working on prem

--- orchestra/db/dao/custom_endpoint_benchmark_dao.py
+++ orchestra/db/dao/custom_endpoint_benchmark_dao.py
@@ -1,12 +1,13 @@
-import datetime
-from typing import List
-from fastapi import Depends
+import os
+from datetime import datetime
 
+from fastapi import Depends
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from orchestra.db.dependencies import get_db_session
 from orchestra.db.models.orchestra_models import CustomEndpointBenchmark
+from orchestra.web.api.utils.on_prem import OnPremModel
 
 
 class CustomEndpointBenchmarkDAO:
@@ -14,22 +15,30 @@ class CustomEndpointBenchmarkDAO:
 
     def __init__(self, session: Session = Depends(get_db_session)):
         self.session = session
+        self.on_prem = os.environ.get("ON_PREM")
+        if self.on_prem:
+            self.on_prem_model = OnPremModel(
+                model_class=CustomEndpointBenchmark,
+                table_name="custom_endpoint_benchmark",
+            )
 
     def upload_benchmark(
         self,
         endpoint_id: str,
         metric_name: str,
         value: float,
-        measured_at: datetime.datetime,
+        measured_at: datetime,
     ):
-        self.session.add(
-            CustomEndpointBenchmark(
-                custom_endpoint_id=endpoint_id,
-                metric_name=metric_name,
-                value=value,
-                measured_at=measured_at,
-            )
-        )
+        data = {
+            "custom_endpoint_id": endpoint_id,
+            "metric_name": metric_name,
+            "value": value,
+            "measured_at": str(measured_at),
+        }
+        if self.on_prem:
+            self.on_prem_model.create(**data)
+        else:
+            self.session.add(CustomEndpointBenchmark(**data))
 
     def benchmarks_between(
         self,
@@ -38,6 +47,21 @@ class CustomEndpointBenchmarkDAO:
         start_time,
         end_time,
     ):
+        if self.on_prem:
+            return self.on_prem_model.read(
+                filters={
+                    "custom_endpoint_benchmark": {
+                        "custom_endpoint_id": endpoint_id,
+                        "metric_name": metric_name,
+                        "measured_at": lambda measured_at: (
+                            datetime.fromisoformat(measured_at)
+                            >= datetime.fromisoformat(start_time)
+                            and datetime.fromisoformat(measured_at)
+                            <= datetime.fromisoformat(end_time)
+                        ),
+                    },
+                },
+            )
         query = (
             select(CustomEndpointBenchmark)
             .where(CustomEndpointBenchmark.custom_endpoint_id == endpoint_id)
