40ca40521f62f7b4c355382a31cbf1fea7f0f1a8
guillesanbri <guillesanbri@users.noreply.github.com>
2024-09-05 11:38:47 +0000

    add test for custom api key related endpoints

--- orchestra/tests/test_custom_endpoints.py
+++ orchestra/tests/test_custom_endpoints.py
@@ -1,5 +1,6 @@
 import asyncio
 import datetime
+import json
 import os
 
 import pytest
@@ -24,6 +25,49 @@ def upload_benchmark(client, endpoint_name, metric_name, value):
     }
     response = client.post(url, params=params, headers=HEADERS)
     return response
+
+
+def _custom_key_in_list(key, list):
+    present = False
+    print(list)
+    for k in list:
+        if k["name"] == key:
+            present = True
+    return present
+
+
+@pytest.mark.anyio
+async def test_custom_api_keys(  # noqa: WPS218, E501
+    client: AsyncClient,
+):
+
+    # create custom api key
+    url = "v0/custom_api_key"
+    params = {"name": "key_1_test", "value": "1234"}
+    response = await client.post(url, params=params, headers=HEADERS)
+    assert response.status_code == 200, response.json()
+
+    # list custom api key
+    url = "v0/custom_api_key/list"
+    response = await client.get(url, headers=HEADERS)
+    assert _custom_key_in_list("key_1_test", json.loads(response.text))
+
+    # rename the api key
+    url = "v0/custom_api_key/rename"
+    params = {"name": "key_1_test", "new_name": "renamed_test"}
+    response = await client.post(url, params=params, headers=HEADERS)
+    url = "v0/custom_api_key/list"
+    response = await client.get(url, headers=HEADERS)
+    assert not _custom_key_in_list("key_1_test", json.loads(response.text))
+    assert _custom_key_in_list("renamed_test", json.loads(response.text))
+
+    # delete the api key
+    url = "v0/custom_api_key"
+    params = {"name": "renamed_test"}
+    response = await client.delete(url, params=params, headers=HEADERS)
+    url = "v0/custom_api_key/list"
+    response = await client.get(url, headers=HEADERS)
+    assert not _custom_key_in_list("renamed_test", json.loads(response.text))
 
 
 @pytest.mark.anyio
