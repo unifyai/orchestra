75a0bb4e13fa866af9cddda07d8edd9f0d48470d
vedpatwardhan <vedpat3@gmail.com>
2024-09-13 18:35:00 +0530

    more changes

--- add_basic_data.py
+++ add_basic_data.py
@@ -1,5 +1,6 @@
+import os
 from sqlalchemy import create_engine, insert
-from cloud_db import get_cloud_sql_data
+from get_cloud_data import get_cloud_sql_data
 from orchestra.db.models.orchestra_models import (
     Modality,
     Task,
@@ -9,8 +10,9 @@ from orchestra.db.models.orchestra_models import (
     Users,
 )
 
-# PostgreSQL database URL
-database_url = f"postgresql://orchestra:orchestra@localhost/orchestra"
+
+orchestra_db_host = os.environ.get("ORCHESTRA_DB_HOST")
+database_url = f"postgresql://orchestra:orchestra@{orchestra_db_host}/orchestra"
 local_engine = create_engine(database_url)
 
 tables = {
@@ -25,7 +27,8 @@ data = get_cloud_sql_data(list(tables.keys()))
 data = {
     table: {"model": tables[table]["model"], "rows": data[table]} for table in tables
 }
-data["users"] = [["clxlmko0900539b72enccyi1o", 0, "", "f", 0, 0, "t"]]
+user_id = os.environ.get("USER_ID")
+data["users"] = [[f"{user_id}", 0, "", "f", 0, 0, "t"]]
 
 with local_engine.connect() as conn:
     for key, content in data.items():
@@ -35,61 +38,3 @@ with local_engine.connect() as conn:
         stmt = insert(model)
         conn.execute(stmt.values(rows))
         conn.commit()
-
-
-# """
-# # the new dataset evaluation stuff
-# just copy over the standard dataset info in the script
-# dataset
-# evaluator
-# stored_prompt
-# dataset_prompt
-# evaluation
-# stored_prompt_extra_field
-# stored_prompt_response
-# judgement
-
-
-# needed but don't populate
-# custom_api_key
-# custom_endpoint
-# custom_router
-# local_endpoint
-# query
-# tags
-# """
-
-
-# import os
-# from litellm import Router
-
-# model_list = [{
-#         "model_name": "gpt-4o",
-#         "litellm_params": { # params for litellm completion/embedding call
-#             "model": "gpt-4o",
-#             "api_key": "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"
-#         },
-#     }, {
-#     "model_name": "gpt-3.5-turbo",
-#     "litellm_params": { # params for litellm completion/embedding call
-#         "model": "gpt-3.5-turbo",
-#         "api_key": "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"
-#     },
-# }]
-# router = Router(
-#     model_list=model_list,
-#     routing_strategy="usage-based-routing-v2", # 👈 KEY CHANGE
-# )
-
-# response = router.completion(
-#     model="gpt-3.5-turbo",
-#     messages=[{"role": "user", "content": "Hey, how's it going?"}]
-# )
-
-# print(response)
-
-# response = router.completion(
-#     model="bad-model",
-#     messages=[{"role": "user", "content": "Hey, how's it going?"}],
-#     # mock_testing_fallbacks=True,
-# )
