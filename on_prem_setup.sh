342d0111e122cce042239235e8573d1fd73d00b8
vedpatwardhan <vedpat3@gmail.com>
2024-09-16 17:56:55 +0530

    moved the orchestra run outside the if block

--- on_prem_setup.sh
+++ on_prem_setup.sh
@@ -4,7 +4,8 @@ if [ ! -f ./first_run ]; then
     alembic upgrade "head"
     /usr/local/bin/python add_on_prem_data.py
     touch ./first_run
-    /usr/local/bin/python -m orchestra
 else
     echo "Container has been restarted. Skipping setup commands."
 fi
+
+/usr/local/bin/python -m orchestra
