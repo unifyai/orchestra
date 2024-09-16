eef97fbe4f349cb53d4d85a373dbb085a375987f
vedpatwardhan <vedpat3@gmail.com>
2024-09-16 17:37:13 +0530

    moved the first run file creation above the orchestra start

--- on_prem_setup.sh
+++ on_prem_setup.sh
@@ -3,8 +3,8 @@
 if [ ! -f ./first_run ]; then
     alembic upgrade "head"
     /usr/local/bin/python add_on_prem_data.py
-    /usr/local/bin/python -m orchestra
     touch ./first_run
+    /usr/local/bin/python -m orchestra
 else
     echo "Container has been restarted. Skipping setup commands."
 fi
