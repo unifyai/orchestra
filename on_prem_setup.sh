3d13c7cf22e1d2e2ca661fac5e1dfd73cf9759e6
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2024-09-16 16:40:28 +0530

    got the setup script to only run on container creation and not on restarts

--- on_prem_setup.sh
+++ on_prem_setup.sh
@@ -1,3 +1,10 @@
-alembic upgrade "head"
-/usr/local/bin/python add_on_prem_data.py
-/usr/local/bin/python -m orchestra
+#!/bin/sh
+
+if [ ! -f /first_run ]; then
+    alembic upgrade "head"
+    /usr/local/bin/python add_on_prem_data.py
+    /usr/local/bin/python -m orchestra
+    touch /first_run
+else
+    echo "Container has been restarted. Skipping setup commands."
+fi
