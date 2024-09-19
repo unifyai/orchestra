ca42a6c1941197b57f1f9be06996763b09d27ed5
infrared1029 <yasserfawzi1029@outlook.com>
2024-09-19 16:59:43 +0300

    remove more redundancy

--- orchestra/web/api/evaluators/views.py
+++ orchestra/web/api/evaluators/views.py
@@ -17,23 +17,7 @@ router = APIRouter()
 # utils
 
 
-template_no_ref = """[System]
-Please act as an impartial judge and evaluate the quality of the response provided by an assistant to the user question displayed below.
-Your job is to evaluate how good the assistant's answer is.
-Your evaluation should consider correctness and helpfulness. Identify any mistakes.
-Be as objective as possible. First provide your explanation, then write down your final rating in the range of
-    - "excellent"
-    - "very good"
-    - "satisfactory"
-    - "bad"
-    - "irrelevant"
-
-After that, you must output your final verdict in JSON by **strictly** following this format:
-{{
-    "assistant_rating": [[RATING]]
-}}
-Do not output anything else after your final verdict, but make sure you do give a verdict, that's the most important part!
-
+template_no_ref = """[
 {class_config}
 
 [start of user question]
