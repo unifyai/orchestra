ed5b3de3a41b4f72df41518a094ce7939bcadfda
Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>
2024-09-30 15:25:34 +0530

    added litellm exceptions, also refactored the base provider and fixed the mistral octoai test (#517) (#518)

--- orchestra/tests/test_llm_queries.py
+++ orchestra/tests/test_llm_queries.py
@@ -26,7 +26,7 @@ MODELS = [
     "llama-3-8b-chat@together-ai",
     "llama-3-8b-chat@aws-bedrock",
     "mistral-7b-instruct-v0.3@mistral-ai",
-    "mistral-7b-instruct-v0.3@octoai",
+    "llama-3.1-8b-chat@octoai",
     "gemini-1.5-flash@vertex-ai",
     "llama-3.1-8b-chat@perplexity-ai",
     "llama-3-8b-chat@groq",
@@ -84,7 +84,7 @@ async def test_text_generation(  # noqa: WPS218, E501
 async def test_fallback_parse(
     client: AsyncClient,
 ) -> None:
-    models = ["claude-3-haiku@anthropic", "mistral-7b-instruct-v0.3@octoai"]
+    models = ["claude-3-haiku@anthropic", "llama-3.1-8b-chat@octoai"]
     endpoint = "/v0/chat/completions"
     data = get_chat_completions_payload_fallback(models, stream=False)
     response = await client.post(endpoint, headers=HEADERS, json=data)
@@ -99,13 +99,13 @@ async def test_fallback_parse(
 async def test_fallback_after_fail(
     client: AsyncClient,
 ) -> None:
-    models = ["FAKE_MODEL@anthropic", "mistral-7b-instruct-v0.3@octoai"]
+    models = ["FAKE_MODEL@anthropic", "llama-3.1-8b-chat@octoai"]
     endpoint = "/v0/chat/completions"
     data = get_chat_completions_payload_fallback(models, stream=False)
     response = await client.post(endpoint, headers=HEADERS, json=data)
     assert response.status_code == status.HTTP_200_OK
     response_json = json.loads(response.text)
-    assert response_json["model"] == "mistral-7b-instruct-v0.3@octoai"
+    assert response_json["model"] == "llama-3.1-8b-chat@octoai"
     check_text_gen_response(response_json, "chat.completion")
     check_text_gen_choice(response_json.get("choices")[0], "message")
     check_text_gen_usage(response_json.get("usage"))
@@ -115,7 +115,9 @@ async def test_fallback_after_fail(
 async def test_arrow_fallback_parse(
     client: AsyncClient,
 ) -> None:
-    model_str = "claude-3-haiku->claude-3-sonnet@anthropic->mistral-7b-instruct-v0.3@openai->octoai"
+    model_str = (
+        "claude-3-haiku->claude-3-sonnet@anthropic->llama-3.1-8b-chat@openai->octoai"
+    )
     endpoint = "/v0/chat/completions"
     data = get_chat_completions_arrow_payload_fallback(model_str, stream=False)
     response = await client.post(endpoint, headers=HEADERS, json=data)
@@ -130,13 +132,13 @@ async def test_arrow_fallback_parse(
 async def test_arrow_fallback_after_fail(
     client: AsyncClient,
 ) -> None:
-    model_str = "FAKE_MODEL@anthropic->mistral-7b-instruct-v0.3@octoai"
+    model_str = "FAKE_MODEL@anthropic->llama-3.1-8b-chat@octoai"
     endpoint = "/v0/chat/completions"
     data = get_chat_completions_arrow_payload_fallback(model_str, stream=False)
     response = await client.post(endpoint, headers=HEADERS, json=data)
     assert response.status_code == status.HTTP_200_OK
     response_json = json.loads(response.text)
-    assert response_json["model"] == "mistral-7b-instruct-v0.3@octoai"
+    assert response_json["model"] == "llama-3.1-8b-chat@octoai"
     check_text_gen_response(response_json, "chat.completion")
     check_text_gen_choice(response_json.get("choices")[0], "message")
     check_text_gen_usage(response_json.get("usage"))
