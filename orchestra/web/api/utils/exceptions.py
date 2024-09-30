8354745ddb1c12e7f3603585599848521c62983b
Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>
2024-09-30 09:08:49 +0000

    added litellm exceptions, also refactored the base provider

---
+++ orchestra/web/api/utils/exceptions.py
@@ -0,0 +1,127 @@
+# this file should be mirroring
+# https://github.com/BerriAI/litellm/blob/main/litellm/exceptions.py
+
+import litellm
+
+
+class Error(Exception):
+    def __init__(self, litellm_error: Exception):
+        self.status_code = litellm_error.status_code
+        self.num_retries = litellm_error.num_retries
+        self.max_retries = litellm_error.max_retries
+
+    def __str__(self):
+        _message = self.message
+        if self.num_retries:
+            _message += f" Retried: {self.num_retries} times"
+        if self.max_retries:
+            _message += f", Max Retries: {self.max_retries}"
+        return _message
+
+    def __repr__(self):
+        _message = self.message
+        if self.num_retries:
+            _message += f" Retried: {self.num_retries} times"
+        if self.max_retries:
+            _message += f", Max Retries: {self.max_retries}"
+        return _message
+
+
+class AuthenticationError(Error):
+    def __init__(self, litellm_error: litellm.AuthenticationError):
+        self.message = litellm_error.message.strip("litellm.AuthenticationError: ")
+        super().__init__(litellm_error)
+
+
+class NotFoundError(Error):
+    def __init__(self, litellm_error: litellm.NotFoundError):
+        self.message = litellm_error.message.strip("litellm.NotFoundError: ")
+        super().__init__(litellm_error)
+
+
+class BadRequestError(Error):
+    def __init__(self, litellm_error: litellm.BadRequestError):
+        self.message = litellm_error.message.strip("litellm.BadRequestError: ")
+        super().__init__(litellm_error)
+
+
+class UnprocessableEntityError(Error):
+    def __init__(self, litellm_error: litellm.UnprocessableEntityError):
+        self.message = litellm_error.message.strip("litellm.UnprocessableEntityError: ")
+        super().__init__(litellm_error)
+
+
+class Timeout(Error):
+    def __init__(self, litellm_error: litellm.Timeout):
+        self.message = litellm_error.message.strip("litellm.Timeout: ")
+        super().__init__(litellm_error)
+
+
+class RateLimitError(Error):
+    def __init__(self, litellm_error: litellm.RateLimitError):
+        self.message = litellm_error.message.strip("litellm.RateLimitError: ")
+        super().__init__(litellm_error)
+
+
+class ContextWindowExceededError(Error):
+    def __init__(self, litellm_error: litellm.ContextWindowExceededError):
+        self.message = litellm_error.message.strip(
+            "litellm.ContextWindowExceededError: ",
+        )
+        super().__init__(litellm_error)
+
+
+class RejectedRequestError(BadRequestError):
+    def __init__(self, litellm_error: litellm.BadRequestError):
+        super().__init__(litellm_error)
+        self.message = self.message.strip("litellm.RejectedRequestError: ")
+
+
+class ContentPolicyViolationError(BadRequestError):
+    def __init__(self, litellm_error: litellm.BadRequestError):
+        super().__init__(litellm_error)
+        self.message = self.message.strip("litellm.ContentPolicyViolationError: ")
+
+
+class ServiceUnavailableError(Error):
+    def __init__(self, litellm_error: litellm.ServiceUnavailableError):
+        self.message = litellm_error.message.strip("litellm.ServiceUnavailableError: ")
+        super().__init__(litellm_error)
+
+
+class InternalServerError(Error):
+    def __init__(self, litellm_error: litellm.InternalServerError):
+        self.message = litellm_error.message.strip("litellm.InternalServerError: ")
+        super().__init__(litellm_error)
+
+
+class APIError(Error):
+    def __init__(self, litellm_error: litellm.APIError):
+        self.message = litellm_error.message.strip("litellm.APIError: ")
+        super().__init__(litellm_error)
+
+
+class APIConnectionError(Error):
+    def __init__(self, litellm_error: litellm.APIConnectionError):
+        self.message = litellm_error.message.strip("litellm.APIConnectionError: ")
+        super().__init__(litellm_error)
+
+
+class APIResponseValidationError(Error):
+    def __init__(self, litellm_error: litellm.APIResponseValidationError):
+        self.message = litellm_error.message.strip(
+            "litellm.APIResponseValidationError: ",
+        )
+        super().__init__(litellm_error)
+
+
+class JSONSchemaValidationError(APIError):
+    def __init__(self, litellm_error: litellm.APIError):
+        super().__init__(litellm_error)
+        self.message = self.message.strip("litellm.JSONSchemaValidationError: ")
+
+
+class UnsupportedParamsError(BadRequestError):
+    def __init__(self, litellm_error: litellm.BadRequestError):
+        super().__init__(litellm_error)
+        self.message = self.message.strip("litellm.UnsupportedParamsError: ")
