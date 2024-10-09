8d46618d44d6e17221edebc1db447c73afb52ee9
guillesanbri <Guillesanbri@hotmail.com>
2024-10-09 11:39:06 +0000

    fix them for good no more commits I promise

--- orchestra/web/api/log/schema.py
+++ orchestra/web/api/log/schema.py
@@ -9,13 +9,11 @@ class LogConfig(BaseModel):
         json_schema_extra="eval-project",
     )
     logs: Dict[str, Any] = Field(
-        description=(
-            "Dictionary containing one or more key:value pairs that ",
-            "will be logged into the platform. Keys can have an optional ",
-            "version defined after a forward slash. E.g. `system_msg/v1`. ",
-            "If defined, these versions will be used when grouping results on ",
-            "a per-key basis. Values must be JSON serializable.",
-        ),
+        description="Dictionary containing one or more key:value pairs that "
+        "will be logged into the platform. Keys can have an optional "
+        "version defined after a forward slash. E.g. `system_msg/v1`. "
+        "If defined, these versions will be used when grouping results on "
+        "a per-key basis. Values must be JSON serializable.",
         json_schema_extra={
             "example": {"input": "...", "score-test-1": "..."},
         },
