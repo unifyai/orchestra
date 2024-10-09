cf359e256ba44a1ca47745e5444ebd6ddf4c855c
guillesanbri <Guillesanbri@hotmail.com>
2024-10-09 10:29:32 +0000

    accept any as part of the logs + improve tests

--- orchestra/web/api/log/schema.py
+++ orchestra/web/api/log/schema.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Any, Dict
 
 from pydantic import BaseModel, Field
 
@@ -11,13 +11,13 @@ class LogConfig(BaseModel):
         ),
         json_schema_extra="eval-project",
     )
-    logs: Dict[str, str] = Field(
+    logs: Dict[str, Any] = Field(
         description=(
             "Dictionary containing one or more key:value pairs that ",
             "will be logged into the platform. Keys can have an optional ",
             "version defined after a forward slash. E.g. `system_msg/v1`. ",
             "If defined, these versions will be used when grouping results on ",
-            "a per-key basis.",
+            "a per-key basis. Values must be JSON serializable.",
         ),
         json_schema_extra={
             "example": {"input": "...", "score-test-1": "..."},
