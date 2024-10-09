9b69144569d23f8715d78bf2c0005f0de6243546
guillesanbri <Guillesanbri@hotmail.com>
2024-10-09 17:42:14 +0000

    cleanup + notes for future cleanup

--- orchestra/db/models/orchestra_models.py
+++ orchestra/db/models/orchestra_models.py
@@ -134,6 +134,7 @@ class Metric(Base):
     plottable = Column(Boolean(), nullable=False)  # type: ignore
 
 
+# CLEANUP: Delete this
 class QueryOld(Base):
     """Model class for the old query table."""
 
@@ -186,6 +187,7 @@ class RechargeType(Base):
     type = Column(String(), primary_key=True)
 
 
+# CLEANUP: Delete this
 class BetaList(Base):
     """Model class for the beta list table."""
 
@@ -368,6 +370,9 @@ class Dataset(Base):
     __table_args__ = (UniqueConstraint("user_id", "name", name="uq_userid_name"),)
 
 
+# TODO: Add DatasetEntry table
+
+# CLEANUP: Delete this
 class StoredPrompt(Base):
     """Model class for the stored prompt table."""
 
@@ -393,6 +398,7 @@ class StoredPrompt(Base):
     )
 
 
+# CLEANUP: Delete this
 class DefaultPrompt(Base):
     """Model class for the default prompt table."""
 
@@ -404,6 +410,7 @@ class DefaultPrompt(Base):
     prompt = Column(String())
 
 
+# CLEANUP: Delete this
 class StoredPromptVariation(Base):
     """Model class for variations of stored prompts table."""
 
@@ -424,6 +431,7 @@ class StoredPromptVariation(Base):
     )
 
 
+# CLEANUP: Delete this
 class StoredPromptResponse(Base):
     """Model class for the stored prompt response table."""
 
@@ -450,6 +458,7 @@ class StoredPromptResponse(Base):
     )
 
 
+# CLEANUP: Delete this
 class Judgement(Base):
     """Model class for the judgement table."""
 
@@ -472,6 +481,7 @@ class Judgement(Base):
     )
 
 
+# CLEANUP: Delete this
 class DatasetPrompt(Base):
     """Model class for the dataset prompt table."""
 
@@ -489,6 +499,7 @@ class DatasetPrompt(Base):
     )
 
 
+# CLEANUP: Delete this
 class Evaluator(Base):
     """Model class for the evaluator table."""
 
@@ -518,6 +529,7 @@ class Evaluator(Base):
     )
 
 
+# CLEANUP: Delete this
 class Evaluation(Base):
     """Model class for the evaluation table."""
 
--- orchestra/web/api/router.py
+++ orchestra/web/api/router.py
@@ -33,15 +33,92 @@ API_KEY_AUTH = [Depends(auth_api_key)]
 ADMIN_AUTH = [Depends(auth_admin_key)] if not os.environ.get("ON_PREM") else None
 
 api_router = APIRouter()
+
+# ADMIN_AUTH endpoints
+
 api_router.include_router(
-    supported_endpoints.router,
-    tags=["Supported Endpoints"],
-    include_in_schema=True,
+    admin.router,
+    prefix="/admin",
+    tags=["admin"],
+    include_in_schema=False,
+    dependencies=ADMIN_AUTH,
+)
+api_router.include_router(  # CLEANUP: Delete this
+    evaluations.admin_router,
+    tags=["Evaluations"],
+    include_in_schema=False,
+    dependencies=ADMIN_AUTH,
+)
+api_router.include_router(  # CLEANUP: Delete this? Check if it's being used
+    provider.router,
+    prefix="/admin",
+    tags=["provider"],
+    include_in_schema=False,
+    dependencies=ADMIN_AUTH,
+)
+api_router.include_router(
+    users.admin_router,
+    prefix="/admin",
+    tags=["Users"],
+    include_in_schema=False,
+    dependencies=ADMIN_AUTH,
+)
+
+# API_KEY_AUTH endpoints
+
+api_router.include_router(
+    artifact.router,
+    tags=["Log"],
+    dependencies=API_KEY_AUTH,
+)
+api_router.include_router(
+    credits.router,
+    tags=["Credits"],
+    dependencies=API_KEY_AUTH,
+)
+api_router.include_router(
+    custom_api_keys.router,
+    tags=["Custom API keys"],
+    dependencies=API_KEY_AUTH,
+)
+api_router.include_router(
+    custom_endpoints.router,
+    tags=["Custom Endpoints"],
+    dependencies=API_KEY_AUTH,
+)
+api_router.include_router(  # CLEANUP: Delete this
+    datasets.router,
+    tags=["Datasets"],
+    dependencies=API_KEY_AUTH,
+)
+api_router.include_router(  # CLEANUP: Delete this
+    default_prompt.router,
+    tags=["Default Prompts"],
+    dependencies=API_KEY_AUTH,
+)
+api_router.include_router(
+    efficiency_benchmarks.router,
+    tags=["Efficiency Benchmarks"],
+    dependencies=API_KEY_AUTH,
+)
+api_router.include_router(  # CLEANUP: Delete this
+    evaluations.router,
+    tags=["Evaluations"],
+    dependencies=API_KEY_AUTH,
+)
+api_router.include_router(  # CLEANUP: Delete this
+    evaluators.router,
+    tags=["Evaluators"],
     dependencies=API_KEY_AUTH,
 )
 api_router.include_router(
     llm_queries.router,
     tags=["LLM Queries"],
+    dependencies=API_KEY_AUTH,
+)
+api_router.include_router(
+    log.router,
+    tags=["Log"],
     dependencies=API_KEY_AUTH,
 )
 api_router.include_router(
@@ -55,59 +132,8 @@ api_router.include_router(
     dependencies=API_KEY_AUTH,
 )
 api_router.include_router(
-    artifact.router,
-    tags=["Log"],
-    dependencies=API_KEY_AUTH,
-)
-api_router.include_router(
-    log.router,
-    tags=["Log"],
-    dependencies=API_KEY_AUTH,
-)
-api_router.include_router(
-    custom_endpoints.router,
-    tags=["Custom Endpoints"],
-    dependencies=API_KEY_AUTH,
-)
-api_router.include_router(
-    custom_api_keys.router,
-    tags=["Custom API keys"],
-    dependencies=API_KEY_AUTH,
-)
-api_router.include_router(
-    datasets.router,
-    tags=["Datasets"],
-    dependencies=API_KEY_AUTH,
-)
-api_router.include_router(
-    evaluators.router,
-    tags=["Evaluators"],
-    dependencies=API_KEY_AUTH,
-)
-api_router.include_router(
-    default_prompt.router,
-    tags=["Default Prompts"],
-    dependencies=API_KEY_AUTH,
-)
-api_router.include_router(
-    evaluations.router,
-    tags=["Evaluations"],
-    dependencies=API_KEY_AUTH,
-)
-api_router.include_router(
-    evaluations.admin_router,
-    tags=["Evaluations"],
-    include_in_schema=False,
-    dependencies=ADMIN_AUTH,
-)
-api_router.include_router(
-    efficiency_benchmarks.router,
-    tags=["Efficiency Benchmarks"],
-    dependencies=API_KEY_AUTH,
-)
-api_router.include_router(
-    router_training.router,
-    tags=["Router Training"],
+    router_configurations.router,
+    tags=["Router Configurations"],
     dependencies=API_KEY_AUTH,
 )
 api_router.include_router(
@@ -116,35 +142,18 @@ api_router.include_router(
     dependencies=API_KEY_AUTH,
 )
 api_router.include_router(
-    router_configurations.router,
-    tags=["Router Configurations"],
+    router_training.router,
+    tags=["Router Training"],
     dependencies=API_KEY_AUTH,
 )
 api_router.include_router(
-    credits.router,
-    tags=["Credits"],
+    supported_endpoints.router,
+    tags=["Supported Endpoints"],
+    include_in_schema=True,
     dependencies=API_KEY_AUTH,
 )
-api_router.include_router(
-    provider.router,
-    prefix="/admin",
-    tags=["provider"],
-    include_in_schema=False,
-    dependencies=ADMIN_AUTH,
-)
-api_router.include_router(
-    admin.router,
-    prefix="/admin",
-    tags=["admin"],
-    include_in_schema=False,
-    dependencies=ADMIN_AUTH,
-)
-api_router.include_router(
-    users.admin_router,
-    prefix="/admin",
-    tags=["Users"],
-    include_in_schema=False,
-    dependencies=ADMIN_AUTH,
-)
+
+# NO AUTH
+
 api_router.include_router(monitoring.router)
 api_router.include_router(docs.router)
