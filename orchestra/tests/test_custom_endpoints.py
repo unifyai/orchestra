1d1e73351cb00f2197ab7f3fe865a1b2042c56a3
Daniel <daniel.lenton11@imperial.ac.uk>
2024-10-13 16:50:05 +0300

    split custom endpoints and endpoint metrics into seperate test files.

--- orchestra/tests/test_custom_endpoints.py
+++ orchestra/tests/test_custom_endpoints.py
@@ -1,5 +1,3 @@
-import asyncio
-import datetime
 import json
 import os
 
@@ -14,17 +12,6 @@ HEADERS = {
     "accept": "application/json",
     "Authorization": f"Bearer {api_key}",
 }
-
-
-def upload_endpoint_metric(client, endpoint_name, metric_name, value):
-    url = "/v0/endpoint-metrics"
-    params = {
-        "endpoint_name": endpoint_name,
-        "metric_name": metric_name,
-        "value": value,
-    }
-    response = client.post(url, params=params, headers=HEADERS)
-    return response
 
 
 def _custom_key_in_list(key, list):
@@ -176,115 +163,3 @@ async def test_create_custom_endpoint_invalid_provider(  # noqa: WPS218, E501
     }
     response = await client.post(url, params=params, headers=HEADERS)
     assert response.status_code == 400, response.json()
-
-
-@pytest.mark.anyio
-async def test_custom_endpoint_metrics(  # noqa: WPS218, E501
-    client: AsyncClient,
-):
-
-    url = "v0/custom_api_key"
-    params = {
-        "name": "dummy_key",
-        "value": "1234",
-    }
-    response = await client.post(url, params=params, headers=HEADERS)
-    assert response.status_code == 200, response.json()
-
-    # create custom endpoint
-    url = "v0/custom_endpoint"
-    params = {
-        "name": "test_custom_endpoint@custom",
-        "url": "https://",
-        "key_name": "dummy_key",
-    }
-    response = await client.post(url, params=params, headers=HEADERS)
-    assert response.status_code == 200, response.json()
-
-    endpoint_name = "test_custom_endpoint@custom"
-    url = "/v0/endpoint-metrics"
-    params = {
-        "endpoint_name": endpoint_name,
-        "metric_name": "time-to-first-token",
-        "value": 135,
-    }
-
-    response = await client.post(url, params=params, headers=HEADERS)
-    assert response.status_code == 200, response.json()
-
-    # now list them
-    endpoint_name = "test_custom_endpoint@custom"
-
-    url = "/v0/endpoint-metrics"
-    params = {
-        "model": endpoint_name.split("@")[0],
-        "provider": endpoint_name.split("@")[1],
-        "start_time": "2024-01-01",
-        "end_time": str(datetime.datetime.now()),
-    }
-    response = await client.get(url, params=params, headers=HEADERS)
-    assert response.status_code == 200, response.json()
-    assert len(response.json()) > 0
-    assert "ttft" in response.json()[0]
-    assert response.json()[0]["ttft"] == 135
-
-
-async def test_custom_endpoint_metrics_get_latest(  # noqa: WPS218, E501
-    client: AsyncClient,
-):
-
-    url = "v0/custom_api_key"
-    params = {
-        "name": "dummy_key",
-        "value": "1234",
-    }
-    response = await client.post(url, params=params, headers=HEADERS)
-    assert response.status_code == 200, response.json()
-
-    # create custom endpoint
-    endpoint_name = "test_custom_endpoint@custom"
-    url = "v0/custom_endpoint"
-    params = {
-        "name": endpoint_name,
-        "url": "https://",
-        "key_name": "dummy_key",
-    }
-    response = await client.post(url, params=params, headers=HEADERS)
-    assert response.status_code == 200, response.json()
-
-    # upload benchmarks
-    response = await upload_endpoint_metric(
-        client,
-        endpoint_name,
-        "time-to-first-token",
-        135,
-    )
-    assert response.status_code == 200, response.json()
-
-    response = await upload_endpoint_metric(
-        client,
-        endpoint_name,
-        "inter-token-latency",
-        500,
-    )
-    assert response.status_code == 200, response.json()
-
-    await asyncio.sleep(5)
-    response = await upload_endpoint_metric(
-        client,
-        endpoint_name,
-        "time-to-first-token",
-        133,
-    )
-    assert response.status_code == 200, response.json()
-
-    # check we return latest
-    url = "/v0/endpoint-metrics"
-    params = {
-        "model": endpoint_name.split("@")[0],
-        "provider": endpoint_name.split("@")[1],
-    }
-    response = await client.get(url, params=params, headers=HEADERS)
-    assert response.status_code == 200, response.json()
-    assert response.json()[0]["ttft"] == 133
-    assert response.json()[0]["itl"] == 500
