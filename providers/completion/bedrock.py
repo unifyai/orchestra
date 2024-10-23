7a52f69a3e1b48b7015844d125d46a5d24a6f8ec
Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>
2024-10-23 06:27:56 +0000

    updated model string to remove cross-region inferencing for the new sonnet model

--- providers/completion/bedrock.py
+++ providers/completion/bedrock.py
@@ -135,7 +135,7 @@ supported_models = {
         "cost": {"prompt": 15, "completion": 75},
     },
     "claude-3.5-sonnet": {
-        "endpoint": "bedrock/us.anthropic.claude-3-5-sonnet-20241022-v2:0",
+        "endpoint": "bedrock/anthropic.claude-3-5-sonnet-20241022-v2:0",
         "context_window": 200000,
         "cost": {"prompt": 3, "completion": 15},
     },
