a63901ca28d370c630391efa2c08e05131bd0b3a
vedpatwardhan <vedpat3@gmail.com>
2024-12-31 18:16:30 +0530

    added the interface endpoint for managing views

---
+++ orchestra/tests/test_interface.py
@@ -0,0 +1,65 @@
+import json
+import os
+
+import pytest
+from httpx import AsyncClient
+
+# Common headers and data
+api_key = str(os.getenv("AUTH_ACCOUNT_API_KEY"))
+HEADERS = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {api_key}",
+}
+
+
+async def _create_interface(client: AsyncClient, items, new_counter):
+    return client.post(
+        "/v0/interface/",
+        headers=HEADERS,
+        json={
+            "items": json.dumps(items),
+            "new_counter": new_counter,
+        },
+    )
+
+
+@pytest.mark.anyio
+async def test_create_interface(client: AsyncClient):
+    items = [
+        {"i": "n0", "x": 0, "y": 0, "w": 3, "h": 3, "tab": None},
+    ]
+    new_counter = 1
+    response = await _create_interface(client, items, new_counter)
+    assert response.status_code == 201, response.json()
+    assert response.json() == ("Interface created successfully!")
+
+
+@pytest.mark.anyio
+async def test_update_interface(client: AsyncClient):
+    items = [
+        {"i": "n0", "x": 0, "y": 0, "w": 3, "h": 3, "tab": None},
+        {"i": "n1", "x": 0, "y": 1, "w": 2, "h": 3, "tab": "Plot_1"},
+    ]
+    new_counter = 2
+    response = await client.put(
+        "/v0/interface/",
+        headers=HEADERS,
+        json={
+            "items": json.dumps(items),
+            "new_counter": new_counter,
+        },
+    )
+    assert response.status_code == 200, response.json()
+    assert response.json() == ("Interface updated successfully!")
+
+
+@pytest.mark.anyio
+async def test_get_interface(client: AsyncClient):
+    items = [
+        {"i": "n0", "x": 0, "y": 0, "w": 3, "h": 3, "tab": None},
+    ]
+    new_counter = 1
+    _create_interface(client, items, new_counter)
+    response = await client.get("/v0/interface/", headers=HEADERS)
+    assert response.status_code == 200, response.json()
+    assert isinstance(response.json(), list)
