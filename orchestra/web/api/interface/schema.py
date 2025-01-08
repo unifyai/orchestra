00350779fc6a6044d310c5e60f64db487fc42478
vedpatwardhan <vedpat3@gmail.com>
2025-01-08 12:42:44 +0530

    added the temp interface table and changes to the interface table and endpoints

--- orchestra/web/api/interface/schema.py
+++ orchestra/web/api/interface/schema.py
@@ -10,8 +10,28 @@ class Item(BaseModel):
     moved: bool
     static: bool
     tab: str | None = None
+    table: str | None = None
+    filters: str | None = None
+    common_filter: str | None = None
+    page_number: str | None = None
+    metric: str | None = None
+    column_order: str | None = None
+    hidden_columns: str | None = None
+    sorting: str | None = None
+    grouping: str | None = None
+    columns_pin_left: str | None = None
+    columns_pin_right: str | None = None
+    selected: str | None = None
+    plot_type: str | None = None
+    plot_scale: str | None = None
+    is_aggregated: str | None = None
+    x_axis: str | None = None
+    y_axis: str | None = None
+    plot_group_by: str | None = None
 
 
 class InterfaceConfig(BaseModel):
     items: list[Item]
     new_counter: int
+    project: str | None = None
+    temporary: bool = False
