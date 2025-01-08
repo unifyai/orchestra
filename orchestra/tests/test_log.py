57791700c66e674a0f8c9466d39476e4f7f0d7c0
Daniel Lenton <dan@unify.ai>
2025-01-08 14:15:41 +0000

    small improvements to sorting logic, and updated sorting test to highlight that the behaviour for the default created_at is not working correctly.

--- orchestra/tests/test_log.py
+++ orchestra/tests/test_log.py
@@ -1214,6 +1214,59 @@ async def test_get_logs_w_sorting(client: AsyncClient):
     _ = await _create_project(client, project_name)
     _ = await _create_several_logs(client, project_name)
 
+    # ascending created_at (default)
+    response = await client.get(
+        f"/v0/logs?project={project_name}",
+        headers=HEADERS,
+    )
+    assert response.status_code == 200, response.json()
+    result = response.json()
+    assert len(result["logs"]) == 7
+    assert result["logs"][0]["entries"] == {
+        "_/description": "boiling water",
+        "_/temperature": 100.0,
+        "_/state": "liquid->gas",
+        "_/safe": False,
+        "_/timestamp": (datetime(1993, 3, 22, tzinfo=timezone.utc)).isoformat(),
+    }
+    assert result["logs"][1]["entries"] == {
+        "_/description": "freezing water",
+        "_/temperature": 0.0,
+        "_/state": "liquid->solid",
+        "_/safe": True,
+        "_/timestamp": (datetime(1993, 3, 22, tzinfo=timezone.utc)).isoformat(),
+    }
+    assert result["logs"][2]["entries"] == {
+        "_/description": "surface of the sun",
+        "_/temperature": 6000.0,
+        "_/state": "gas",
+        "_/safe": False,
+        "_/timestamp": (datetime(1993, 3, 22, tzinfo=timezone.utc)).isoformat(),
+    }
+    assert result["logs"][3]["entries"] == {
+        "_/description": "freezing nitrogen",
+        "_/temperature": -210.0,
+        "_/state": "liquid->solid",
+        "_/safe": False,
+        "_/timestamp": (datetime(1993, 3, 22, tzinfo=timezone.utc)).isoformat(),
+    }
+    assert result["logs"][4]["entries"] == {
+        "_/description": "lava",
+        "_/metadata": [1, 5, 6],
+        "_/_data": {"a": 2, "b": 4},
+        "_/timestamp": (datetime(1993, 3, 24, tzinfo=timezone.utc)).isoformat(),
+    }
+    assert result["logs"][5]["entries"] == {
+        "_/description": "air",
+        "_/metadata": [3, 8, 5],
+        "_/_data": {"a": 6, "b": 12, "c": 8, "d": 11},
+        "_/timestamp": (datetime(1993, 3, 24, tzinfo=timezone.utc)).isoformat(),
+    }
+    assert result["logs"][6]["entries"] == {
+        "_/_data": {"a": 8, "b": 10},
+        "_/timestamp": (datetime(1993, 3, 24, tzinfo=timezone.utc)).isoformat(),
+    }
+
     # ascending temperature
     response = await client.get(
         f"/v0/logs?project={project_name}",
@@ -1362,23 +1415,23 @@ async def test_get_logs_w_sorting(client: AsyncClient):
         "_/timestamp": (datetime(1993, 3, 22, tzinfo=timezone.utc)).isoformat(),
     }
     assert result["logs"][4]["entries"] == {
-        "_/description": "freezing nitrogen",
-        "_/temperature": -210.0,
-        "_/state": "liquid->solid",
-        "_/safe": False,
-        "_/timestamp": (datetime(1993, 3, 22, tzinfo=timezone.utc)).isoformat(),
-    }
-    assert result["logs"][5]["entries"] == {
         "_/description": "freezing water",
         "_/temperature": 0.0,
         "_/state": "liquid->solid",
         "_/safe": True,
         "_/timestamp": (datetime(1993, 3, 22, tzinfo=timezone.utc)).isoformat(),
     }
-    assert result["logs"][6]["entries"] == {
+    assert result["logs"][5]["entries"] == {
         "_/description": "surface of the sun",
         "_/temperature": 6000.0,
         "_/state": "gas",
+        "_/safe": False,
+        "_/timestamp": (datetime(1993, 3, 22, tzinfo=timezone.utc)).isoformat(),
+    }
+    assert result["logs"][6]["entries"] == {
+        "_/description": "freezing nitrogen",
+        "_/temperature": -210.0,
+        "_/state": "liquid->solid",
         "_/safe": False,
         "_/timestamp": (datetime(1993, 3, 22, tzinfo=timezone.utc)).isoformat(),
     }
@@ -1405,22 +1458,25 @@ async def test_get_logs_w_sorting(client: AsyncClient):
         "_/timestamp": (datetime(1993, 3, 24, tzinfo=timezone.utc)).isoformat(),
     }
     assert result["logs"][2]["entries"] == {
-        "_/description": "surface of the sun",
-        "_/temperature": 6000.0,
-        "_/state": "gas",
-        "_/safe": False,
-        "_/timestamp": (datetime(1993, 3, 22, tzinfo=timezone.utc)).isoformat(),
-    }
-    assert result["logs"][3]["entries"] == {
         "_/description": "boiling water",
         "_/temperature": 100.0,
         "_/state": "liquid->gas",
         "_/safe": False,
         "_/timestamp": (datetime(1993, 3, 22, tzinfo=timezone.utc)).isoformat(),
     }
+    assert result["logs"][3]["entries"] == {
+        "_/description": "freezing water",
+        "_/temperature": 0.0,
+        "_/state": "liquid->solid",
+        "_/safe": True,
+        "_/timestamp": (datetime(1993, 3, 22, tzinfo=timezone.utc)).isoformat(),
+    }
     assert result["logs"][4]["entries"] == {
-        "_/_data": {"a": 8, "b": 10},
-        "_/timestamp": (datetime(1993, 3, 24, tzinfo=timezone.utc)).isoformat(),
+        "_/description": "surface of the sun",
+        "_/temperature": 6000.0,
+        "_/state": "gas",
+        "_/safe": False,
+        "_/timestamp": (datetime(1993, 3, 22, tzinfo=timezone.utc)).isoformat(),
     }
     assert result["logs"][5]["entries"] == {
         "_/description": "freezing nitrogen",
@@ -1430,11 +1486,8 @@ async def test_get_logs_w_sorting(client: AsyncClient):
         "_/timestamp": (datetime(1993, 3, 22, tzinfo=timezone.utc)).isoformat(),
     }
     assert result["logs"][6]["entries"] == {
-        "_/description": "freezing water",
-        "_/temperature": 0.0,
-        "_/state": "liquid->solid",
-        "_/safe": True,
-        "_/timestamp": (datetime(1993, 3, 22, tzinfo=timezone.utc)).isoformat(),
+        "_/_data": {"a": 8, "b": 10},
+        "_/timestamp": (datetime(1993, 3, 24, tzinfo=timezone.utc)).isoformat(),
     }
 
 
