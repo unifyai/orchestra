43d594b7b10d13067c117edc40fdc1149f098b8e
YushaArif99 <YushaArif99@users.noreply.github.com>
2025-04-13 21:48:35 +0500

    Add constants for SQL type mappings in python2SQL

---
+++ orchestra/web/api/log/python2SQL/constants.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Boolean, Date, DateTime, Float, Integer, Interval, String, Time
+from sqlalchemy.dialects.postgresql import JSONB
+
+__all__ = ["STR_TO_SQL_TYPES"]
+
+STR_TO_SQL_TYPES = {
+    "bool": Boolean,
+    "int": Integer,
+    "float": Float,
+    "str": String,
+    "timestamp": DateTime,
+    "time": Time,
+    "date": Date,
+    "timedelta": Interval,
+    "dict": JSONB,
+    "list": JSONB,
+}
