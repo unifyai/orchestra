5c07d8a8e6d3e912529f4b66bec0ecde37a56014
YushaArif99 <YushaArif99@users.noreply.github.com>
2025-05-07 22:04:24 +0500

    Update down_revision in migration file to reflect the latest schema changes

--- orchestra/db/migrations/versions/2025-05-07-21-42_236616a2d695.py
+++ orchestra/db/migrations/versions/2025-05-07-21-42_236616a2d695.py
@@ -11,7 +11,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = "236616a2d695"
-down_revision = "46c2450d64eb"
+down_revision = "1bfcb03cb98a"
 branch_labels = None
 depends_on = None
 
