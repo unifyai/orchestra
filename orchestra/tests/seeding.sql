6355c1bf44aeecad1c06fe6c267c819259daf7ff
vedpatwardhan <vedpat3@gmail.com>
2025-05-20 11:21:01 +0530

    weekly update of models and providers

--- orchestra/tests/seeding.sql
+++ orchestra/tests/seeding.sql
@@ -19,7 +19,6 @@ INSERT INTO recharge_type VALUES ('free');
 INSERT INTO provider VALUES (1, 'openai', '', '');
 INSERT INTO provider VALUES (3, 'deepinfra', '', '');
 INSERT INTO provider VALUES (4, 'fireworks-ai', '', '');
-INSERT INTO provider VALUES (5, 'lepton-ai', '', '');
 INSERT INTO provider VALUES (6, 'replicate', '', '');
 INSERT INTO provider VALUES (7, 'together-ai', '', '');
 INSERT INTO provider VALUES (8, 'mistral-ai', '', '');
@@ -71,7 +70,6 @@ INSERT INTO model VALUES (15, 'grok-2', NOW(), 'dummy_task', true);
 
 INSERT INTO endpoint VALUES (1, 9, 3, NOW(), true);
 INSERT INTO endpoint VALUES (3, 13, 4, NOW(), true);
-INSERT INTO endpoint VALUES (4, 13, 5, NOW(), true);
 INSERT INTO endpoint VALUES (5, 9, 6, NOW(), true);
 INSERT INTO endpoint VALUES (6, 13, 7, NOW(), true);
 INSERT INTO endpoint VALUES (7, 11, 8, NOW(), true);
