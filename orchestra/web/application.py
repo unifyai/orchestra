e4e325e8e772bb9ae1f735dfafe03d054dd6613f
YushaArif99 <YushaArif99@users.noreply.github.com>
2025-05-23 12:37:28 +0500

    Re-enable Prometheus and Production Traffic middlewares in application.py as its not affecting latency

--- orchestra/web/application.py
+++ orchestra/web/application.py
@@ -11,7 +11,10 @@ from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
 from orchestra.settings import settings
 from orchestra.web.api.router import api_router
-from orchestra.web.api.utils.prometheus_middleware import metrics
+from orchestra.web.api.utils.production_traffic_middleware import (
+    ProductionTrafficMiddleware,
+)
+from orchestra.web.api.utils.prometheus_middleware import PrometheusMiddleware, metrics
 from orchestra.web.lifetime import register_shutdown_event, register_startup_event
 
 
@@ -53,15 +56,15 @@ def get_app() -> FastAPI:
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
-        # )
-        # # Add Prometheus metrics middleware
-        # app.add_middleware(
-        #     PrometheusMiddleware,
-        #     app_name="orchestra",
-        # )
-        # # Add Production Traffic middleware
-        # app.add_middleware(
-        #     ProductionTrafficMiddleware,
+    )
+    # Add Prometheus metrics middleware
+    app.add_middleware(
+        PrometheusMiddleware,
+        app_name="orchestra",
+    )
+    # Add Production Traffic middleware
+    app.add_middleware(
+        ProductionTrafficMiddleware,
     )
     # Register startup and shutdown events
     register_startup_event(app)
