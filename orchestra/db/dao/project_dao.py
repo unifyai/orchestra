cb985c59783af28eb89cfe530e6be4592e8787ec
YushaArif99 <YushaArif99@users.noreply.github.com>
2025-06-11 11:05:53 +0500

    Enhance ProjectVersion model by adding version attribute and update ProjectDAO to correctly track project version during instantiation and rollback operations.

--- orchestra/db/dao/project_dao.py
+++ orchestra/db/dao/project_dao.py
@@ -192,6 +192,7 @@ class ProjectDAO:
 
         project_version = ProjectVersion(
             project_id=project_id,
+            version=project.version,
             commit_hash=commit_hash,
             commit_message=commit_message,
         )
