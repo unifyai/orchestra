2f3424727dc7780f6317c509bf1557e7096a7105
nassimberrada <112006029+nassimberrada@users.noreply.github.com>
2025-06-17 15:41:54 +0100

    Manually set credentials file when loading bucket service (#994)
    ---------
    
    Co-authored-by: nassimberrada <Nassim>

--- orchestra/services/bucket_service.py
+++ orchestra/services/bucket_service.py
@@ -8,6 +8,7 @@ from typing import Optional, Tuple
 
 from google.api_core import exceptions
 from google.cloud import storage
+from google.oauth2 import service_account
 
 from orchestra.web.api.utils.gcp import parse_gcs_url
 
@@ -15,12 +16,25 @@ from orchestra.web.api.utils.gcp import parse_gcs_url
 class BucketService:
     def __init__(self):
         """Initialize the bucket service with GCP credentials and bucket configuration."""
+
+        self.credentials_path = os.getenv("ORCHESTRA_VERTEXAI_SERVICE_ACC_JSON")
+        if not self.credentials_path:
+            raise ValueError(
+                "Missing required GCP credentials key (ORCHESTRA_VERTEXAI_SERVICE_ACC_JSON)",
+            )
+
         self.project_id = os.getenv("ORCHESTRA_VERTEXAI_PROJECT")
         if not self.project_id:
             raise ValueError(
                 "Missing required GCP project ID configuration (ORCHESTRA_VERTEXAI_PROJECT)",
             )
-        self.storage_client = storage.Client(project=self.project_id)
+
+        self.credentials = service_account.Credentials.from_service_account_file(
+            self.credentials_path
+        )
+        self.storage_client = storage.Client(
+            project=self.project_id, credentials=self.credentials
+        )
 
         # Generic bucket
         self.bucket_name = os.getenv("ORCHESTRA_GCP_BUCKET_NAME")
